1e6e3ee520233195a83b7708dadf409169270b62
dipu-bd <dipu.sudipta@gmail.com>
2018-01-28 08:22:33 +0600

    Oopify lnmtl

--- lnmtl.py
+++ lnmtl.py
@@ -5,55 +5,99 @@
 [LNMTL](https://lnmtl.com) is a website containing machine translated
 novels. This code will convert any given book from this site into epub.
 """
-import sys
-from os import path, makedirs
 import re
-import json
-from splinter import Browser
+import sys
+from os import path
 from binding import novel_to_kindle
+from helper import get_browser, save_chapter
 
-home_url = 'https://lnmtl.com/'
-login_url = 'https://lnmtl.com/auth/login/'
-logout_url = 'https://lnmtl.com/auth/logout'
-email = 'dipu@algomatrix.co'
-password = 'twill1123'
 
-def get_browser():
-    executable_path = path.join('lib', 'chromedriver')
-    return Browser('chrome',
-                   headless=True,
-                   incognito=True,
-                   executable_path=executable_path)
-# end def
+class LNMTLCrawler:
+    '''Crawler for LNMTL'''
 
-def start():
-    if login():
-        crawl_pages(start_url)
-        logout()
-    else:
-        print('Failed to login')
-    # end with
-# end def
+    def __init__(self, novel_id, start_url=None, end_url=None):
+        if not novel_id:
+            raise Exception('Novel ID is required')
+        # end if
 
-def login():
-    print('Attempting login:', login_url)
-    browser.visit(login_url)
-    browser.find_by_css('form input#email').fill(email)
-    browser.find_by_css('form input#password').fill(password)
-    browser.find_by_css('form button[type="submit"]').click()
-    return browser.url == home_url
-# end def
+        self.novel_id = novel_id
+        self.author_name = 'Sudipto Chandra'
 
-def logout():
-    print('Attempting logout:', logout_url)
-    browser.visit(logout_url)
-    return browser.url == home_url
-# end def
+        self.home_url = 'https://lnmtl.com'
+        self.login_url = 'https://lnmtl.com/auth/login'
+        self.logout_url = 'https://lnmtl.com/auth/logout'
+        self.email = 'dipu@algomatrix.co'
+        self.password = 'twill1123'
 
-def crawl_pages(url):
-    while url:
-        print('Visiting:', url)
-        browser.visit(url)
+        self.start_url = self.get_url_from_chapter(start_url)
+        self.end_url = self.get_url_from_chapter(end_url)
+
+        self.output_path = path.join('_novel', novel_id)
+    # end def
+
+    def get_url_from_chapter(self, chapter):
+        '''get url from chapter number'''
+        if not chapter:
+            return None
+        # end if
+        if chapter.isdigit():
+            return '%s/%s-chapter-%s' % (self.home_url, self.novel_id, chapter)
+        else:
+            return chapter.strip('/')
+    # end def
+
+    def start(self):
+        '''start crawling'''
+        browser = get_browser()
+        try:
+            if not self.start_url:
+                raise Exception('No start url')
+            # end if
+            if not self.login(browser):
+                raise Exception('Failed to login')
+            # end if
+            self.crawl_chapters(browser)
+            self.logout(browser)
+        finally:
+            browser.quit()
+        # end try
+        novel_to_kindle(self.output_path)
+    # end def
+
+    def login(self, browser):
+        '''login to LNMTL'''
+        print('Attempting login:', self.login_url)
+        browser.visit(self.login_url)
+        browser.find_by_css('form input#email').fill(self.email)
+        browser.find_by_css('form input#password').fill(self.password)
+        browser.find_by_css('form button[type="submit"]').click()
+        return browser.url == self.home_url
+    # end def
+
+    def logout(self, browser):
+        '''logout as a good citizen'''
+        print('Attempting logout:', self.logout_url)
+        browser.visit(self.logout_url)
+        return browser.url == self.home_url
+    # end def
+
+    def crawl_chapters(self, browser):
+        '''Crawl all chapters till the end'''
+        url = self.start_url
+        while url:
+            print('Visiting:', url)
+            browser.visit(url)
+            self.parse_chapter(browser)
+            if url == self.end_url:
+                break
+            # end if
+            url = browser.find_by_css('nav .pager .next a').first['href']
+        # end while
+    # end def
+
+    def parse_chapter(self, browser):
+        '''Parse the content of the chapter page'''
+        url = browser.url
         # parse contents
         titles = browser.find_by_css('div.dashhead-titles')
         novel = titles.find_by_css('.dashhead-subtitle a')[0]['title']
@@ -61,8 +105,8 @@ def crawl_pages(url):
         chapter = titles.find_by_css('.dashhead-title').first.text
         translated = browser.find_by_css('.chapter-body .translated')
         # format contents
-        chapter = format_text(chapter)
-        body = '\n'.join([format_text(x.text) for x in translated if x.text.strip()])
+        body = '\n'.join([self.format_text(x.text) for x in translated if x.text.strip()])
+        chapter = self.format_text(chapter)
         volume_no = re.search(r'\d+$', volume).group()
         chapter_no = re.search(r'\d+$', url).group()
         # save data
@@ -73,46 +117,24 @@ def crawl_pages(url):
             'chapter_no': chapter_no,
             'chapter_title': chapter,
             'body': '<h1>%s</h1>%s' % (chapter, body)
-        })
-        # move on to next
-        if url.strip('/') == end_url.strip('/'): break
-        url = browser.find_by_css('nav .pager .next a').first['href']
-    # end while
-# end def
+        }, self.output_path)
+    # end def
 
-def format_text(text):
-    text = text.replace(u'\u00ad', '')
-    text = re.sub(r'\u201e[, ]*', '&ldquo;', text)
-    text = re.sub(r'\u201d[, ]*', '&rdquo;', text)
-    # text = re.sub(r'[^\x00-\x7f]', r'', text)
-    return '<p>' + text.strip() + '</p>'
-# end def
+    def format_text(self, text):
+        '''formats the text and remove bad characters'''
+        text = text.replace(u'\u00ad', '')
+        text = re.sub(r'\u201e[, ]*', '&ldquo;', text)
+        text = re.sub(r'\u201d[, ]*', '&rdquo;', text)
+        text = re.sub(r'[ ]*,[ ]+', ', ', text)
+        return '<p>' + text.strip() + '</p>'
+    # end def
+# end class
 
-def save_chapter(content):
-    # save to file
-    vol = content['volume_no'].rjust(2, '0')
-    chap = content['chapter_no'].rjust(5, '0')
-    file_name = path.join(output_path, vol, chap + '.json')
-    if not path.exists(path.dirname(file_name)):
-        makedirs(path.dirname(file_name))
-    # end if
-    with open(file_name, 'w') as file:
-        file.write(json.dumps(content))
-    # end with
-# end def
 
 if __name__ == '__main__':
-    novel_id = sys.argv[1]
-    start_url = sys.argv[2]
-    end_url = sys.argv[3] if len(sys.argv) > 3 else ''
-    output_path = path.join('_novel', novel_id)
-
-    browser = get_browser()
-    try:
-        start()
-    finally:
-        browser.quit()
-    # end try
-
-    novel_to_kindle(output_path)
+    LNMTLCrawler(
+        novel_id=sys.argv[1],
+        start_url=sys.argv[2] if len(sys.argv) > 2 else '',
+        end_url=sys.argv[3] if len(sys.argv) > 3 else ''
+    ).start()
 # end if
