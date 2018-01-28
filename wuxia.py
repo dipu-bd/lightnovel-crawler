b15d3afdf83bbbf4a307ebd351a42284f96b69e2
dipu-bd <dipu.sudipta@gmail.com>
2018-01-28 07:56:31 +0600

    Oopify the wuxia.py

--- wuxia.py
+++ wuxia.py
@@ -12,10 +12,9 @@ import json
 from splinter import Browser
 from binding import novel_to_kindle
 
-novel_name = 'Unknown'
-
 
 def get_browser():
+    '''open a headless chrome browser in incognito mode'''
     executable_path = path.join('lib', 'chromedriver')
     return Browser('chrome',
                    headless=True,
@@ -23,85 +22,125 @@ def get_browser():
                    executable_path=executable_path)
 # end def
 
-def start():
-    try:
-        meta_info()
-        crawl_pages(start_url)
-    finally:
-        browser.quit()
-    # end try
-# end def
 
-def meta_info():
-    global novel_name
-    print('Visiting:', home_url)
-    browser.visit(home_url)
-    novel_name = browser.find_by_css('h1.entry-title').first.text
-    novel_name = re.sub(r' – Index$', '', novel_name)
-# end def
+class WuxiaCrawler:
+    '''Crawler for WuxiaWorld'''
 
-def crawl_pages(url):
-    while url:
-        print('Visiting:', url)
-        browser.visit(url)
-        next_link = browser.find_link_by_partial_text('Next Chapter')
-        if not next_link: break
-        # parse contents
+    def __init__(self, novel_id, start_url=None, end_url=None):
+        if not novel_id:
+            raise Exception('Novel ID is required')
+        # end if
+
+        self.novel_id = novel_id
+        self.novel_name = 'Unknown'
+        self.author_name = 'Sudipto Chandra'
+
+        self.home_url = 'http://www.wuxiaworld.com/%s-index' % (novel_id)
+        self.start_url = self.get_url_from_chapter(start_url)
+        self.end_url = self.get_url_from_chapter(end_url)
+
+        self.output_path = path.join('_novel', novel_id)
+    # end def
+
+    def get_url_from_chapter(self, chapter):
+        '''get url from chapter number'''
+        if not chapter:
+            return None
+        # end if
+        if chapter.isdigit():
+            return '%s/%s-chapter-%s' % (self.home_url, self.novel_id, chapter)
+        else:
+            return chapter.strip('/')
+    # end def
+
+    def start(self):
+        '''start crawling'''
+        browser = get_browser()
+        try:
+            if self.start_url:
+                self.crawl_meta_info(browser)
+                self.crawl_chapters(browser)
+            # end if
+        finally:
+            browser.quit()
+        # end try
+        novel_to_kindle(self.output_path)
+    # end def
+
+    def crawl_meta_info(self, browser):
+        '''get novel name and author'''
+        print('Visiting:', self.home_url)
+        browser.visit(self.home_url)
+        novel = browser.find_by_css('h1.entry-title').first.text
+        self.novel_name = re.sub(r' – Index$', '', novel)
+    # end def
+
+    def crawl_chapters(self, browser):
+        '''Crawl all chapters till the end'''
+        url = self.start_url
+        while url:
+            print('Visiting:', url)
+            browser.visit(url)
+            next_link = browser.find_link_by_partial_text('Next Chapter')
+            if not next_link:
+                break
+            # end if
+            self.parse_chapter(browser)
+            if url == self.end_url:
+                break
+            # end if
+            url = next_link.first['href'].strip('/')
+        # end while
+    # end def
+
+    def parse_chapter(self, browser):
+        '''Parse the content of the chapter page'''
+        url = browser.url.strip('/')
         chapter_no = re.search(r'\d+.?$', url).group().strip('/')
         vol_no = str(1 + (int(chapter_no) - 1) // 100)
         if re.match(r'.*-book-\d+-chapter-\d+', url):
             vol_no = re.search(r'-\d+-', url).group().strip('-')
         # end if
         articles = browser.find_by_css('div[itemprop="articleBody"] p')
-        chapter_title = articles[1].text
-        if re.match(r'Chapter \d+.*', articles[2].text):
-            chapter_title = articles[2].text
+        body = [x for x in articles][1:-1]
+        chapter_title = body[0].text
+        if re.match(r'Chapter \d+.*', body[1].text):
+            chapter_title = body[1].text
+            body = body[2:]
+        else:
+            body = body[1:]
         # end if
-        body = [x for i, x in enumerate(articles) if 0 < i < len(articles) - 1]
-        body = '\n'.join(['<p>' + x.html + '</p>' for x in body])
+        body = ''.join(['<p>' + x.html + '</p>' for x in body if x.text.strip()])
         # save data
-        save_chapter({
+        self.save_chapter({
             'url': url,
-            'novel': novel_name,
+            'novel': self.novel_name,
             'chapter_no': chapter_no,
             'chapter_title': chapter_title,
             'volume_no': vol_no,
-            'body': body
+            'body': '<h1>%s</h1>%s' % (chapter_title, body)
         })
-        # move on to next
-        if url == end_url: break
-        url = next_link.first['href'].strip('/')
-    # end while
-# end def
+    # end def
 
-def save_chapter(content):
-    # save to file
-    vol = content['volume_no'].rjust(2, '0')
-    chap = content['chapter_no'].rjust(5, '0')
-    file_name = path.join(output_path, vol, chap + '.json')
-    if not path.exists(path.dirname(file_name)):
-        makedirs(path.dirname(file_name))
-    # end if
-    with open(file_name, 'w') as file:
-        file.write(json.dumps(content))
-    # end with
-# end def
+    def save_chapter(self, content):
+        '''save content to file'''
+        vol = content['volume_no'].rjust(2, '0')
+        chap = content['chapter_no'].rjust(5, '0')
+        file_name = path.join(self.output_path, vol, chap + '.json')
+        if not path.exists(path.dirname(file_name)):
+            makedirs(path.dirname(file_name))
+        # end if
+        print('Saving ', file_name)
+        with open(file_name, 'w') as file:
+            file.write(json.dumps(content))
+        # end with
+    # end def
+# end class
 
 if __name__ == '__main__':
-    novel_id = sys.argv[1]
-    start_url = sys.argv[2].strip('/')
-    end_url = sys.argv[3].strip('/') if len(sys.argv) > 3 else ''
-
-    output_path = path.join('_novel', novel_id)
-    home_url = 'http://www.wuxiaworld.com/%s-index' % (novel_id)
-    if start_url.isdigit():
-        start_url = '%s/%s-chapter-%s' % (home_url, novel_id, start_url)
-    # end if
-    if end_url.isdigit():
-        end_url = '%s/%s-chapter-%s' % (home_url, novel_id, end_url)
-    # end if
-
-    browser = get_browser()
-    start()
-    novel_to_kindle(output_path)
+    WuxiaCrawler(
+        novel_id=sys.argv[1],
+        start_url=sys.argv[2] if len(sys.argv) > 2 else None,
+        end_url=sys.argv[2] if len(sys.argv) > 3 else None
+    ).start()
 # end if
