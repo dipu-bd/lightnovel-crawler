c84db640fd63798bdf34fdee5e4fc7c879493d3e
dipu-bd <dipu.sudipta@gmail.com>
2018-04-18 23:08:17 +0600

    Make upto get_chapter_bodies

--- EbookCrawler/readln.py
+++ EbookCrawler/readln.py
@@ -5,10 +5,10 @@ Crawler for novels from [ReadLightNovel](https://www.readlightnovel.org).
 """
 import re
 import sys
-import json
 import requests
 from os import path
 import concurrent.futures
+from bs4 import BeautifulSoup
 from .binding import novel_to_kindle
 from .helper import get_browser, save_chapter
 
@@ -22,56 +22,65 @@ class ReadLightNovelCrawler:
             raise Exception('Novel ID is required')
         # end if
 
+        self.chapters = []
         self.novel_id = novel_id
         self.start_chapter = start_chapter
         self.end_chapter = end_chapter
-        self.chapters = []
 
+        self.home_url = 'https://www.readlightnovel.org'
         self.output_path = path.join('_novel', novel_id)
-    # end def
 
+        requests.urllib3.disable_warnings()
+    # end def
 
     def start(self):
         '''start crawling'''
         self.get_chapter_list()
-        self.get_chapter_bodies()
+        # self.get_chapter_bodies()
         novel_to_kindle(self.output_path)
-    # end def
-
-    def get_csrf_token(self):
-        '''get csrf token'''
-        # document.querySelectorAll('.chapters .chapter-chs li a').forEach(v => console.log(v.href))
-        url = 'https://www.webnovel.com/book/' + self.novel_id
-        print('Getting CSRF Token from ', url)
-        session = requests.Session()
-        session.get(url)
-        cookies = session.cookies.get_dict()
-        self.csrf = cookies['_csrfToken']
-        print('CSRF Token =', self.csrf)
     # end def
 
     def get_chapter_list(self):
         '''get list of chapters'''
-        url = 'https://www.webnovel.com/apiajax/chapter/GetChapterList?_csrfToken=' \
-              + self.csrf + '&bookId=' + self.novel_id
-        print('Getting book name and chapter list...')
-        response = requests.get(url)
+        url = '%s/%s' % (self.home_url, self.novel_id)
+        print('Visiting ', url)
+        response = requests.get(url, verify=False)
         response.encoding = 'utf-8'
-        data = response.json()
-        self.chapters = [x['chapterId'] for x in data['data']['chapterItems']]
-        print(len(self.chapters), 'chapters found')
+        html_doc = response.text
+        print('Getting book name and chapter list... ')
+        soup = BeautifulSoup(html_doc, 'lxml')
+        # get book name
+        self.novel_name = soup.select_one('.block-title h1').text
+        # get chapter list
+        self.chapters = [x.get('href') for x in soup.select('.chapters .chapter-chs li a')]
+        print(' [%s]' % self.novel_name, len(self.chapters), 'chapters found')
+    # end def
+
+    def get_chapter_index(self, chapter):
+      if not chapter: return None
+      if chapter.isdigit():
+        if 0 < chapter <= len(self.chapters):
+          return chapter - 1
+        else:
+          raise Exception('Invalid chapter number')
+        # end if
+      # end if
+      for i, link in enumerate(self.chapters):
+        if chapter == link:
+          return i
+        # end if
+      # end for
+      raise Exception('Invalid chapter url')
     # end def
 
     def get_chapter_bodies(self):
         '''get content from all chapters till the end'''
+        self.start_chapter = self.get_chapter_index(self.start_chapter)
+        self.end_chapter = self.get_chapter_index(self.end_chapter)
         if not self.start_chapter: return
         start = int(self.start_chapter)
         end = int(self.end_chapter or len(self.chapters))
-        start = max(start - 1, 0)
         end = min(end, len(self.chapters))
-        if start >= len(self.chapters):
-          return print('ERROR: start chapter out of bound.')
-        # end if
         future_to_url = {self.executor.submit(self.parse_chapter, index):\
             index for index in range(start, end)}
         # wait till finish
@@ -79,19 +88,16 @@ class ReadLightNovelCrawler:
     # end def
 
     def parse_chapter(self, index):
-        chapter_id = self.chapters[index]
-        url = 'https://www.webnovel.com/apiajax/chapter/GetContent?_csrfToken=' \
-            + self.csrf + '&bookId=' + self.novel_id + '&chapterId=' + chapter_id
-        print('Getting chapter...', index + 1, '[' + chapter_id + ']')
-        response = requests.get(url)
+        url = self.chapters[index]
+        print('Visiting ', url)
+        response = requests.get(url, verify=False)
         response.encoding = 'utf-8'
-        data = response.json()
-        novel_name = data['data']['bookInfo']['bookName']
-        author_name = data['data']['bookInfo']['authorName']
-        chapter_title = data['data']['chapterInfo']['chapterName']
-        chapter_no = data['data']['chapterInfo']['chapterIndex']
-        contents = data['data']['chapterInfo']['content']
-        body_part = self.format_text(contents)
+        html_doc = response.text
+        print('Getting chapter body... ')
+        soup = BeautifulSoup(html_doc, 'lxml')
+        chapter_title = soup.select_one('.block-title h1').text
+        chapter_no = index + 1
+        body_part = self.select('.chapter-content3 p')
         volume_no = ((chapter_no - 1) // 100) + 1
         chapter_title = '#%d: %s' % (chapter_no, chapter_title)
         save_chapter({
@@ -125,7 +131,7 @@ class ReadLightNovelCrawler:
 # end class
 
 if __name__ == '__main__':
-    WebNovelCrawler(
+    ReadLightNovelCrawler(
         novel_id=sys.argv[1],
         start_chapter=sys.argv[2] if len(sys.argv) > 2 else None,
         end_chapter=sys.argv[3] if len(sys.argv) > 3 else None
