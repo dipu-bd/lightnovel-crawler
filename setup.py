345e66cd20165a4f9dfcebb276cee890965c4191
dipu-bd <dipu.sudipta@gmail.com>
2018-07-13 01:42:03 +0600

    Complete setup.py

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['src'],  # Optional
+    install_requires=['EbookCrawler'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
@@ -182,7 +182,7 @@ setup(
     # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         'console_scripts': [
-            '__main__=__main__:main',
+            'ebook-crawler=EbookCrawler:main',
         ],
     },
 
