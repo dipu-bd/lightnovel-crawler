3e7f69c56ab8b8d91551f7decaa827d9635eeda4
dipu-bd <dipu.sudipta@gmail.com>
2018-07-12 06:28:26 +0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='EbookCrawler',
+      version='1.4',
+      description='Crawls light novels and make epub and mobi',
+      author='Sudipto Chandra',
+      author_email='dipu.sudipta@gmail.com',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Programming Language :: Python'
+      ],
+      packages=[
+        
+      ])
