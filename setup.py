45dff7d4e1f48e100e96295321e867239436a553
dipu-bd <dipu.sudipta@gmail.com>
2018-07-13 02:29:55 +0600

    No extra-require

--- setup.py
+++ setup.py
@@ -152,10 +152,10 @@ setup(
     #
     # Similar to `install_requires` above, these must be valid existing
     # projects.
-    extras_require={  # Optional
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
+    # extras_require={  # Optional
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.
