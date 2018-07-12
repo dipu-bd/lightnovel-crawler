3b6991e2b724e34a193a55cbaa51c22c6d416d8c
dipu-bd <dipu.sudipta@gmail.com>
2018-07-13 02:32:34 +0600

    Use requirements.txt in setup.py

--- setup.py
+++ setup.py
@@ -21,6 +21,10 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+# Get the long description from the README file
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    install_requires = f.readlines()
+
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
@@ -142,7 +146,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['ebook_crawler'],  # Optional
+    install_requires=install_requires + ['ebook_crawler'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
