b9c64a117f11dba80681930a05ce54f8751e802f
dipu-bd <dipu.sudipta@gmail.com>
2018-08-21 12:58:03 +0600

    Improve and fix binding

--- ebook_crawler/binding.py
+++ ebook_crawler/binding.py
@@ -21,49 +21,33 @@ KINDLEGEN_PATH_LINUX = os.path.join(DIR_NAME, 'ext', 'kindlegen-linux')
 KINDLEGEN_PATH_WINDOWS = os.path.join(DIR_NAME, 'ext', 'kindlegen-windows')
 SYSTEM_OS = platform.system()
 
-def novel_to_kindle(input_path, pack_by_volume):
+def novel_to_epub(input_path, pack_by_volume):
     ''''Convert novel to epub'''
     if not os.path.exists(input_path):
         return print(input_path, 'does not exists')
     # end if
-    novel_id = os.path.basename(input_path)
     # Create epubs by volumes
-    output_path = None
     if not pack_by_volume:
-        try:
-            output_path = bind_book(input_path, novel_id)
-        except:
-            print('!! Failed to bind:', input_path)
-        # end try
+        _bind_book(input_path)
     else :
-        for volume_no in sorted(os.listdir(input_path)):
-            try:
-                output_path = bind_book(input_path, novel_id, volume_no)
-            except:
-                print('!! Failed to bind:', input_path, volume_no)
-            # end try
+        json_path = os.path.join(input_path, 'json')
+        for volume_no in sorted(os.listdir(json_path)):
+            _bind_book(input_path, volume_no)
         # end for
     #end if
-    if output_path:
-        convert_to_mobi(output_path)
-    # end if
 # end def
 
-def bind_book(input_path, novel_id, volume_no = ''):
+def _bind_book(input_path, volume_no = ''):
     # create book
     book = epub.EpubBook()
-    book.set_identifier(novel_id + volume_no)
     book.set_language('en')
-    vol = ''
-    if volume_no and volume_no != '':
-        vol = ' Volume ' + volume_no.rjust(2, '0')
-    # end if
+    book.set_identifier(input_path + volume_no)
     # get chapters
     contents = []
-    book_title = None
-    book_author = None
-    book_cover = None
-    full_vol = os.path.join(input_path, volume_no)
+    book_title = 'N/A'
+    book_author = 'N/A'
+    book_cover = 'N/A'
+    full_vol = os.path.join(input_path, 'json', volume_no)
     print('Processing:', full_vol)
     for file_name in sorted(os.listdir(full_vol)):
         # read data
@@ -79,16 +63,13 @@ def bind_book(input_path, novel_id, volume_no = ''):
             title=item['chapter_title'])
         book.add_item(chapter)
         contents.append(chapter)
-        if not book_title: book_title = item['novel']
-        if not book_cover: book_cover = item['cover']
-        if not book_author and 'author' in item: book_author = item['author']
+        book_title = item['novel'] if 'novel' in item else book_title
+        book_cover = item['cover'] if 'cover' in item else book_cover
+        book_author = item['author'] if 'author' in item else book_author
     # end for
-    book_title = book_title or 'N/A'
-    book_cover = book_cover or 'N/A'
-    book_author = book_author or 'N/A'
-    book.spine = ['cover','nav'] + contents
     book.add_author(book_author)
-    if book_cover and book_cover != 'N/A':
+    book.spine = ['nav'] + contents
+    if book_cover != 'N/A':
         try:
             filename=book_cover.split('/')[-1]
             opener = urllib.request.build_opener()
@@ -96,53 +77,58 @@ def bind_book(input_path, novel_id, volume_no = ''):
             urllib.request.install_opener(opener)
             urllib.request.urlretrieve(book_cover, filename)
             book.set_cover("image.jpg", open(filename, 'rb').read())
+            book.spine = ['cover'] + book.spine
         except:
             print('Failed to add cover:', book_cover)
             pass
         # end try
-    #endif
-    book.set_title(book_title + vol)
+    # end if
+    if volume_no and volume_no != '':
+        vol = ' Volume ' + volume_no.rjust(2, '0')
+        book.set_title(book_title + vol)
+    else:
+        book.set_title(book_title)
+    # end if
     book.toc = contents
     book.add_item(epub.EpubNav())
     book.add_item(epub.EpubNcx())
-    # Create epub
-    book_title = re.sub('[\\\\/*?:"<>|]' or r'[\\/*?:"<>|]',"", book_title)
-    output_path = book_title
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    # Save epub file
+    epub_path = os.path.join(input_path, 'epub')
+    if not os.path.exists(epub_path):
+        os.makedirs(epub_path)
     # end if
     file_name = book_title + '_v' + volume_no + '.epub'
-    file_path = os.path.join(output_path, file_name)
+    file_path = os.path.join(epub_path, file_name)
     print('Creating:', file_path)
     epub.write_epub(file_path, book, {})
-    return output_path
+    return epub_path
 # end def
 
-def convert_to_mobi(output_path):
+def novel_to_mobi(input_path):
+    epub_path = os.path.join(input_path, 'epub')
     # Convert to mobi format
-    for file_name in sorted(os.listdir(output_path)):
+    for file_name in sorted(os.listdir(epub_path)):
         if not file_name.endswith('.epub'):
             continue
         # end if
-        epub_file = os.path.join(output_path, file_name)
+        epub_file = os.path.join(epub_path, file_name)
+        generator = lambda kindlegen: call([kindlegen, '-c2', epub_file])
         try:
-            kindlegen = None
             if SYSTEM_OS == 'Linux':
-                kindlegen = KINDLEGEN_PATH_LINUX
+                generator(KINDLEGEN_PATH_LINUX)
             elif SYSTEM_OS == 'Darwin':
-                kindlegen = KINDLEGEN_PATH_MAC
+                generator(KINDLEGEN_PATH_MAC)
             elif SYSTEM_OS == 'Windows':
-                kindlegen = KINDLEGEN_PATH_WINDOWS
+                generator(KINDLEGEN_PATH_WINDOWS)
             else:
                 raise Exception('KindleGen does not support this OS.')
             # end if
-            call([kindlegen, '-c2', epub_file])
         except:
             try:
-                call(['kindlegen', '-c2', epub_file])
+                generator('kindlegen')
             except (OSError, Exception) as err:
                 if err[1].errno == errno.ENOENT:
-                    print("Warning: kindlegen was not on your path; not generating .mobi version")
+                    print("Warning: kindlegen was not on your path; not generating .mobi files")
                     print("Visit https://www.amazon.com/gp/feature.html?ie=UTF8&docId=1000765211 to install it.")
                 else:
                     print("-" * 60)
@@ -151,6 +137,19 @@ def convert_to_mobi(output_path):
                 # end if
             # end try
         # end try
+    # end for
+
+    # Move mobi files to mobi_path
+    mobi_path = os.path.join(input_path, 'mobi')
+    if not os.path.exists(mobi_path):
+        os.makedirs(mobi_path)
+    # end if
+
+    for file_name in sorted(os.listdir(epub_path)):
+        if not file_name.endswith('.mobi'):
+            continue
+        # end if
+        os.rename(os.path.join(epub_path, file_name), os.path.join(mobi_path, file_name))
     # end for
 # end def
 
