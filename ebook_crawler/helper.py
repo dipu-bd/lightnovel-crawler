275db3f1b6f21aadc2e00c21a9568afd59f8fcf2
dipu-bd <dipu.sudipta@gmail.com>
2018-08-21 13:26:34 +0600

    Use my style

--- ebook_crawler/helper.py
+++ ebook_crawler/helper.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Helper methods used in crawling"""
+import os
 import json
-from os import path, makedirs
 
 def save_chapter(content, output_path, pack_by_volume=True):
     '''save content to file'''
-    dir_name = path.join(output_path, 'json')
+    dir_name = os.path.join(output_path, 'json')
     if pack_by_volume:
         vol = content['volume_no'].rjust(2, '0')
-        dir_name = path.join(dir_name, vol)
+        dir_name = os.path.join(dir_name, vol)
     # end if
     try:
-        makedirs(dir_name)
+        os.makedirs(dir_name)
     except:
         pass
     # end if
     chap = content['chapter_no'].rjust(5, '0')
-    file_name = path.join(dir_name, chap + '.json')
+    file_name = os.path.join(dir_name, chap + '.json')
     print('Saving ', file_name)
     with open(file_name, 'w') as file:
         file.write(json.dumps(content))
