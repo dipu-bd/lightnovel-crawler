6768f89a92b83bc94a92fb3845640792359ba88d
dipu-bd <dipu.sudipta@gmail.com>
2018-11-07 18:03:44 +0600

    Design the starter app layout

--- ebook_crawler/__init__.py
+++ ebook_crawler/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """Interactive value input"""
+import sys
+import logging
+import urllib3
 from PyInquirer import prompt
 from .lnmtl import LNMTLCrawlerApp
 # from .wuxia import WuxiaCrawler
@@ -10,12 +13,37 @@ from .lnmtl import LNMTLCrawlerApp
 # from .readln import ReadLightNovelCrawler
 # from .novelplanet import NovelPlanetCrawler
 
+def configure():
+    mode = sys.argv[1].lower() if len(sys.argv) > 1 else None
+    if mode == '-v' or mode == '--verbose':
+        print('\33[91m 🔊 IN VERBOSE MODE\33[0m')
+        print('-' * 60)
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    # end if
+# end def
+
+def headline():
+    with open('VERSION', 'r') as f:
+        version = f.read().strip()
+    # end with
+    print('-' * 60)
+    print(' \33[1m\33[92m📒', 'Ebook Crawler 🍀', version, '\33[0m')
+    print(' 🔗\33[94m https://github.com/dipu-bd/site-to-epub', '\33[0m')
+    print(' 🙏\33[94m https://saythanks.io/to/dipu-bd', '\33[0m')
+    print('-' * 60)
+# end def
+
 def main():
-    questions = [
+    headline()
+    configure()
+
+    answer = prompt([
         {
             'type': 'list',
             'name': 'source',
-            'message': 'Where is your novel from?',
+            'message': 'Where is the novel from?',
             'choices': [
                 'https://lnmtl.com',
                 'https://boxnovel.com',
@@ -26,18 +54,8 @@ def main():
                 'https://www.readlightnovel.org'
             ]
         },
-    ]
+    ])
 
-    with open('VERSION', 'r') as f:
-        version = f.read().strip()
-    # end with
-    print('-' * 60)
-    print(' \33[1m\033[92m🍀', 'Ebook Crawler', version, '\33[0m')
-    print(' 🔗\033[94m https://github.com/dipu-bd/site-to-epub', '\033[0m')
-    print(' 🙏\033[94m https://saythanks.io/to/dipu-bd', '\033[0m')
-    print('-' * 60)
-
-    answer = prompt(questions)
     if 'https://lnmtl.com' == answer['source']:
         LNMTLCrawlerApp().start()
     elif 'https://boxnovel.com' == answer['source']:
