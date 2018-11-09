f1b4806c6559fc69b1ad7511a506cba0b281b2c9
dipu-bd <dipu.sudipta@gmail.com>
2018-11-09 17:14:34 +0600

    Moved get_choice method to the app.__init__

--- ebook_crawler/__init__.py
+++ ebook_crawler/__init__.py
@@ -4,8 +4,9 @@
 import sys
 import logging
 import requests
-from PyInquirer import prompt
-from .app import run_app
+
+from .app import start_app
+
 from .lnmtl import LNMTLCrawler
 from .webnovel import WebnovelCrawler
 from .wuxia import WuxiaCrawler
@@ -15,8 +16,10 @@ from .readln import ReadLightNovelCrawler
 from .novelplanet import NovelPlanetCrawler
 from .lnindo import LnindoCrawler
 from .idqidian import IdqidianCrawler
+from .utils.crawler import Crawler
 
-choices = {
+
+crawler_list = {
     'https://lnmtl.com': LNMTLCrawler,
     'https://www.webnovel.com': WebnovelCrawler,
     'https://www.wuxiaworld.com': WuxiaCrawler,
@@ -34,9 +37,9 @@ def configure():
         print('\33[91m 🔊 IN VERBOSE MODE\33[0m')
         print('-' * 60)
         logging.basicConfig(level=logging.DEBUG)
-    else:
-        requests.urllib3.disable_warnings(
-            requests.urllib3.exceptions.InsecureRequestWarning)
+    # end if
+    requests.urllib3.disable_warnings(
+        requests.urllib3.exceptions.InsecureRequestWarning)
     # end if
 # end def
 
@@ -44,6 +47,7 @@ def headline():
     with open('VERSION', 'r') as f:
         version = f.read().strip()
     # end with
+    print('\033c', end='')
     print('-' * 60)
     print(' \33[1m\33[92m📒', 'Ebook Crawler 🍀', version, '\33[0m')
     print(' 🔗\33[94m https://github.com/dipu-bd/site-to-epub', '\33[0m')
@@ -51,47 +55,11 @@ def headline():
     print('-' * 60)
 # end def
 
-def get_choice():
-    answer = prompt([
-        {
-            'type': 'list',
-            'name': 'source',
-            'message': 'Where is the novel from?',
-            'choices': choices.keys(),
-        },
-    ])
-
-    choice = choices[answer['source']]
-    if not choice:
-        print('\n  Not yet implemented  \n')
-        return None
-    # end if
-    return choice()
-# end def
-
 def main():
     headline()
     configure()
 
-    error = False
-    while True:
-        try:
-            crawler = get_choice()
-            run_app(crawler)
-            error = False
-            print('-' * 80, end='\n\n')
-            print('Bye... Come back soon!')
-            break
-        except Exception as ex:
-            raise ex # TODO: remove when on production
-            if error:
-                break
-            else:
-                error = True
-                print('\n⮕ Press \33[95mCtrl + C\33[0m again to exit\n')
-            # end if
-        # end try
-    # end if
+    start_app(crawler_list)
 # end def
 
 if __name__ == '__main__':
