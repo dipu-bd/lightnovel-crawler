dfcceed6faeb3a7ba11a7024521ff2dde5e2da60
Sudipto Chandra <dipu.sudipta@gmail.com>
2018-12-01 01:28:10 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -117,9 +117,9 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
 
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: Microsoft :: Windows',
