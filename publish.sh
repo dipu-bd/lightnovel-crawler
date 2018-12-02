76a5dbaa48d315fab539fcca6b0e4f0561e0ed5e
Sudipto Chandra <dipu.sudipta@gmail.com>
2018-12-02 13:24:32 +0600

    Automate the asset building

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@ VERSION=$(head -n 1 lightnovel-crawler/VERSION)
 
 git clean -xfd
 
-python3 setup.py bdist_wheel
-python3 setup.py sdist
+python setup.py bdist_wheel
+python setup.py sdist
 
 twine upload "dist/lightnovel_crawler-$VERSION-py3-none-any.whl"
