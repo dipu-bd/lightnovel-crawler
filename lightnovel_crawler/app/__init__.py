3db37785dab877fc0801afd53a6dbab3b91f0de3
Sudipto Chandra <dipu.sudipta@gmail.com>
2018-12-05 10:58:09 +0600

    app: Raise exception on unrecognized url

--- lightnovel_crawler/app/__init__.py
+++ lightnovel_crawler/app/__init__.py
@@ -84,7 +84,7 @@ def start_app(choice_list):
 
         if not instance:
             url_not_recognized(choice_list)
-            return
+            raise Exception('URL is not recognized')
         # end if
 
         Program().run(instance)
