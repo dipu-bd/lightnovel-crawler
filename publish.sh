7a7a86902fc509fcac02a0efbf56b9d81e637e08
Sudipto Chandra Dipu <dipu.sudipta@gmail.com>
2019-01-09 13:48:55 +0600

    Use python3 for publishing

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@ VERSION=$(head -n 1 VERSION)
 git clean -xfd
 
 export build_assets=true
-python setup.py bdist_wheel sdist
+python3 setup.py bdist_wheel sdist
 
 twine upload "dist/lightnovel_crawler-$VERSION-py3-none-any.whl"
