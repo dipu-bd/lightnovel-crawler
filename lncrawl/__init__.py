12a40e218ac10296548635681a822efbcf3af58b
Sudipto Chandra Dipu <dipu.sudipta@gmail.com>
2019-01-20 15:11:29 +0600

    Add env files and bot_requirements.txt

--- lncrawl/__init__.py
+++ lncrawl/__init__.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from dotenv import load_dotenv
+
 from .core import start_app
 from .utils.crawler import Crawler
+from .bots.telegram import telegram_bot
+
+load_dotenv()
 
 def main():
     start_app()
 # end def
-
 
 if __name__ == '__main__':
     main()
