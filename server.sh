7bc24adc2b2e13b016f77efac0f4e460d85678bd
Sudipto Chandra <dipu.sudipta@gmail.com>
2019-01-24 22:28:02 +0600

    Add server.sh

---
+++ server.sh
@@ -0,0 +1,14 @@
+###################################################
+# Run this script in server to keep alive the bot #
+###################################################
+
+git checkout master
+git pull origin master
+
+kill -9 `cat save_pid.txt`
+rm save_pid.txt
+
+nohup python3.5 . > bot.log 2>&1 &
+echo $! > save_pid.txt
+
+tail -f bot.log
