603085bc781831a68a5e0434bd306d5c9e6e1c51
Sudipto Chandra Dipu <dipu.sudipta@gmail.com>
2019-02-19 11:28:47 +0600

    Back to python3

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@ VERSION=$(head -n 1 VERSION)
 
 rm -rf build dist *.egg-info
 
-python setup.py bdist_wheel sdist
-python setup_win.py
+python3 setup.py bdist_wheel sdist
+python3 setup_win.py
 
 twine upload "dist/lightnovel_crawler-$VERSION-py3-none-any.whl"
