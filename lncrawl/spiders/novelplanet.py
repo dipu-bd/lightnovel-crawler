9abb0fa4008ddd653aa6f085fd1342e1b9e723b8
Sudipto Chandra <dipu.sudipta@gmail.com>
2019-03-15 16:47:13 +0600

    Fix a typo

--- lncrawl/spiders/novelplanet.py
+++ lncrawl/spiders/novelplanet.py
@@ -24,7 +24,7 @@ class NovelPlanetCrawler(Crawler):
             results.append({
                 'title': a.text.strip(),
                 'url': self.absolute_url(a['href']),
-                'info': 'Lastest: %s' % info,
+                'info': 'Latest: %s' % info,
             })
         # end for
         return results
