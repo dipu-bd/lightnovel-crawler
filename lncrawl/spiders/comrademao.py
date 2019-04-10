3a3ee52b5d2504295ce1202e300177186dbb7dc8
Sudipto Chandra Dipu <dipu.sudipta@gmail.com>
2019-04-10 14:50:41 +0600

    comrademao.py: Cleanup and reformat code

--- lncrawl/spiders/comrademao.py
+++ lncrawl/spiders/comrademao.py
@@ -9,13 +9,12 @@ from ..utils.crawler import Crawler
 from bs4 import BeautifulSoup
 
 logger = logging.getLogger('COMRADEMAO')
+
 #search_url = 'http://novelfull.com/search?keyword=%s'
+chapter_list_url = 'https://comrademao.com/wp-admin/admin-ajax.php?action=movie_datatables&p2m=%s&length=%s'
 
 
 class ComrademaoCrawler(Crawler):
-
-    #temp_chapters = []
-
     def read_novel_info(self):
         '''Get novel title, autor, cover etc'''
         logger.debug('Visiting %s', self.novel_url)
@@ -24,70 +23,47 @@ class ComrademaoCrawler(Crawler):
         self.novel_title = soup.find('h4').text.strip()
         logger.info('Novel title: %s', self.novel_title)
 
-        self.novel_cover = self.absolute_url(soup.find('div',{'class':'wrap-thumbnail'}).find('img')['src'])
+        self.novel_cover = self.absolute_url(
+            soup.find('div', {'class': 'wrap-thumbnail'}).find('img')['src'])
         logger.info('Novel cover: %s', self.novel_cover)
 
-        self.novel_author = soup.find('div',{'class':'author'}).text.strip()
+        self.novel_author = soup.find('div', {'class': 'author'}).text.strip()
         logger.info('Novel author: %s', self.novel_author)
 
-        #page_count = soup.find('span',{'class':'dots'}).findNext('a').text
-        #page_count = -1 if not page_count else int(page_count)
-        #logger.info('Chapter list pages: %d' % page_count)
+        shortlink = soup.find('link', {'rel': 'shortlink'})['href']
+        p_id = re.findall(r'p=(\d+)', shortlink)[0]
 
-        the_url = soup.find("link",{"rel":"shortlink"})['href']
-        p_id = urllib.parse.parse_qs(urllib.parse.urlparse(the_url).query)['p'][0]
-        js = self.scrapper.post("https://comrademao.com/wp-admin/admin-ajax.php?action=movie_datatables&p2m=%s" % p_id)
-        data = js.json()
-        
+        response = self.scrapper.post(chapter_list_url % (p_id, 1))
+        data = response.json()
+
         chapter_count = data['recordsTotal']
-        url = "https://comrademao.com/wp-admin/admin-ajax.php?action=movie_datatables&p2m=%s&length=%s" % (p_id,chapter_count) 
-        js = self.scrapper.post(url)
-        data = js.json()
+        response = self.scrapper.post(chapter_list_url % (p_id, chapter_count))
+        data = response.json()
 
         logger.info('Getting chapters...')
-        chapters = []
-        for chapter in data['data']:
-            link = BeautifulSoup(chapter[1],'lxml').find('a')
-            chapters.append(link)
-        # end for
+        for chapter in reversed(data['data']):
+            a = BeautifulSoup(chapter[1], 'lxml').find('a')
 
-        chapters.reverse()
-
-        for x in chapters:
             chap_id = len(self.chapters) + 1
+            vol_id = chap_id // 100 + 1
+
             if len(self.chapters) % 100 == 0:
-                vol_id = chap_id//100 + 1
-                vol_title = 'Volume ' + str(vol_id)
-                self.volumes.append({
-                    'id': vol_id,
-                    'title': vol_title,
-                })
+                self.volumes.append({'id': vol_id, 'title': ''})
             # end if
+
             self.chapters.append({
                 'id': chap_id,
                 'volume': vol_id,
-                'url': self.absolute_url(x['href']),
-                'title': x.text.strip() or ('Chapter %d' % chap_id),
+                'url': self.absolute_url(a['href']),
+                'title': a.text.strip(),
             })
         # end for
 
         logger.debug(self.chapters)
-        logger.debug('%d chapters found', len(self.chapters))
         logger.debug(self.volumes)
-        logger.info('%d volumes and %d chapters found' % (len(self.volumes), len(self.chapters)))
-    # end def
 
-    #def download_chapter_list(self, page, p_id):
-    #    '''Download list of chapters and volumes.'''
-    #    url = "https://comrademao.com/wp-admin/admin-ajax.php?action=movie_datatables&p2m=%s&draw=%s" % (p_id,page) 
-    #    js = self.scrapper.post(url)
-    #    data = js.json()
-    #    mini_chapters = []
-    #    for chapter in data['data']:
-    #        link = BeautifulSoup(chapter[1],'lxml').find('a')
-    #        mini_chapters.append(link)
-    #    logger.debug('Crawling chapters url in page %s' % page)
-    #    return mini_chapters
+        logger.info('%d volumes and %d chapters found',
+                    len(self.volumes), len(self.chapters))
     # end def
 
     def download_chapter_body(self, chapter):
@@ -95,23 +71,23 @@ class ComrademaoCrawler(Crawler):
         logger.info('Downloading %s', chapter['url'])
         soup = self.get_soup(chapter['url'])
         logger.debug(soup.title.string)
-        
-        #if soup.select('div.entry-content div.container div.container a p'):
+
+        # if soup.select('div.entry-content div.container div.container a p'):
         #    contents = soup.select('div.entry-content div.container div.container a p')
-        #else:
+        # else:
         #    contents = soup.select('div.entry-content div.container a p')
-        
+
         #contents = soup.select('div.entry-content div p')
         #body_parts = []
-        #for x in contents:
+        # for x in contents:
         #    body_parts.append(x.text)
-        
-        #return '<p>' + '</p><p>'.join(body_parts) + '</br></p>'
+
+        # return '<p>' + '</p><p>'.join(body_parts) + '</br></p>'
         #contents = soup.select_one('div.entry-content div')
-        if soup.find("div", attrs={"readability":True}):
-            contents = soup.find("div", attrs={"readability":True})
+        if soup.find('div', attrs={'readability': True}):
+            contents = soup.find('div', attrs={'readability': True})
         else:
-            contents = soup.find("article")
+            contents = soup.find('article')
         # end if
         for item in contents.findAll('div'):
             item.decompose()
