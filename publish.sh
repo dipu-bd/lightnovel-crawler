899a1dddd8aa2802b5467bffca9e7afef7a2b9e2
Sudipto Chandra Dipu <dipu.sudipta@gmail.com>
2019-05-26 11:15:24 +0600

    Update git tag on publish

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 VERSION=$(head -n 1 VERSION)
 
-rm -rf build dist *.egg-info
+rm -rf build dist *.egg-info __pycache__
 
 python3 setup.py bdist_wheel sdist
 
 twine upload "dist/lightnovel_crawler-$VERSION-py3-none-any.whl"
+
+git tag -a "v$VERSION" -m "Version $VERSION"
