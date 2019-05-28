ca8821ca79ef061f038d296aa611eebda53a14b5
Yudi Santoso <lee140685@gmail.com>
2019-05-28 08:54:01 +0700

    Revert before using js to get list of chapter
    Revert to old version where crawler should visit each toc page to get chapter list

--- lncrawl/spiders/comrademao.py
+++ lncrawl/spiders/comrademao.py
@@ -2,19 +2,17 @@
 # -*- coding: utf-8 -*-
 import re
 import logging
-import json
-import urllib.parse
 from concurrent import futures
 from ..utils.crawler import Crawler
-from bs4 import BeautifulSoup
 
 logger = logging.getLogger('COMRADEMAO')
-
 #search_url = 'http://novelfull.com/search?keyword=%s'
-chapter_list_url = 'https://comrademao.com/wp-admin/admin-ajax.php?action=movie_datatables&p2m=%s&length=%s'
 
 
 class ComrademaoCrawler(Crawler):
+
+    #temp_chapters = []
+
     def read_novel_info(self):
         '''Get novel title, autor, cover etc'''
         logger.debug('Visiting %s', self.novel_url)
@@ -23,47 +21,55 @@ class ComrademaoCrawler(Crawler):
         self.novel_title = soup.find('h4').text.strip()
         logger.info('Novel title: %s', self.novel_title)
 
-        self.novel_cover = self.absolute_url(
-            soup.find('div', {'class': 'wrap-thumbnail'}).find('img')['src'])
+        self.novel_cover = self.absolute_url(soup.find('div',{'class':'wrap-thumbnail'}).find('img')['src'])
         logger.info('Novel cover: %s', self.novel_cover)
 
-        self.novel_author = soup.find('div', {'class': 'author'}).text.strip()
+        self.novel_author = soup.find('div',{'class':'author'}).text.strip()
         logger.info('Novel author: %s', self.novel_author)
 
-        shortlink = soup.find('link', {'rel': 'shortlink'})['href']
-        p_id = re.findall(r'p=(\d+)', shortlink)[0]
-
-        response = self.scrapper.post(chapter_list_url % (p_id, 1))
-        data = response.json()
-
-        chapter_count = data['recordsTotal']
-        response = self.scrapper.post(chapter_list_url % (p_id, chapter_count))
-        data = response.json()
-
+        page_count = soup.find('span',{'class':'dots'}).findNext('a').text
+        page_count = -1 if not page_count else int(page_count)
+        logger.info('Chapter list pages: %d' % page_count)
+        
         logger.info('Getting chapters...')
-        for chapter in reversed(data['data']):
-            a = BeautifulSoup(chapter[1], 'lxml').find('a')
+        chapters = []
+        for i in range(page_count):
+            chapters.extend(self.download_chapter_list(i+1))
+        # end for
 
+        chapters.reverse()
+
+        for x in chapters:
             chap_id = len(self.chapters) + 1
-            vol_id = (chap_id - 1) // 100 + 1
-
             if len(self.chapters) % 100 == 0:
-                self.volumes.append({'id': vol_id, 'title': ''})
+                vol_id = chap_id//100 + 1
+                vol_title = 'Volume ' + str(vol_id)
+                self.volumes.append({
+                    'id': vol_id,
+                    'title': vol_title,
+                })
             # end if
-
             self.chapters.append({
                 'id': chap_id,
                 'volume': vol_id,
-                'url': self.absolute_url(a['href']),
-                'title': a.text.strip(),
+                'url': self.absolute_url(x['href']),
+                'title': x.text.strip() or ('Chapter %d' % chap_id),
             })
         # end for
 
         logger.debug(self.chapters)
+        logger.debug('%d chapters found', len(self.chapters))
         logger.debug(self.volumes)
+        logger.info('%d volumes and %d chapters found' % (len(self.volumes), len(self.chapters)))
+    # end def
 
-        logger.info('%d volumes and %d chapters found',
-                    len(self.volumes), len(self.chapters))
+    def download_chapter_list(self, page):
+        '''Download list of chapters and volumes.'''
+        url = self.novel_url.split('?')[0].strip('/')
+        url += '/page/%s/' % page
+        soup = self.get_soup(url)
+        logger.debug('Crawling chapters url in page %s' % page)
+        return soup.select('tbody td a')
     # end def
 
     def download_chapter_body(self, chapter):
@@ -71,23 +77,23 @@ class ComrademaoCrawler(Crawler):
         logger.info('Downloading %s', chapter['url'])
         soup = self.get_soup(chapter['url'])
         logger.debug(soup.title.string)
-
-        # if soup.select('div.entry-content div.container div.container a p'):
+        
+        #if soup.select('div.entry-content div.container div.container a p'):
         #    contents = soup.select('div.entry-content div.container div.container a p')
-        # else:
+        #else:
         #    contents = soup.select('div.entry-content div.container a p')
-
+        
         #contents = soup.select('div.entry-content div p')
         #body_parts = []
-        # for x in contents:
+        #for x in contents:
         #    body_parts.append(x.text)
-
-        # return '<p>' + '</p><p>'.join(body_parts) + '</br></p>'
+        
+        #return '<p>' + '</p><p>'.join(body_parts) + '</br></p>'
         #contents = soup.select_one('div.entry-content div')
-        if soup.find('div', attrs={'readability': True}):
-            contents = soup.find('div', attrs={'readability': True})
+        if soup.find("div", attrs={"readability":True}):
+            contents = soup.find("div", attrs={"readability":True})
         else:
-            contents = soup.find('article')
+            contents = soup.find("article")
         # end if
         for item in contents.findAll('div'):
             item.decompose()
