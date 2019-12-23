40e42d15f9f84a1dd0118a339d1459db2c601dd7
Sudipto Chandra <dipu.sudipta@gmail.com>
2019-12-23 20:45:27 +0600

    [zenithnovels] fix chapter list ordering

--- src/spiders/zenithnovels.py
+++ src/spiders/zenithnovels.py
@@ -42,7 +42,7 @@ class ZenithNovelsCrawler(Crawler):
             # end if
         # end if
 
-        self.chapters.sort(key=lambda x: x['id'])
+        self.chapters.sort(key=lambda x: x['volume'] * 1e6 + x['id'])
         self.volumes = [{'id': x, 'title': ''} for x in set(self.volumes)]
 
         logger.info('%d volumes and %d chapters found',
