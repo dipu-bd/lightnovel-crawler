6ec24d1a10a66711c291b2bc6a79e8d70575e121
Sudipto Chandra <dipu.sudipta@gmail.com>
2019-12-24 12:16:27 +0600

    [test] Refactor the test bot

---
+++ src/bots/test/__init__.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+The purpose of this bot is to test the application and crawlers
+"""
+import traceback
+from random import random
+
+from ...spiders import crawler_list
+from ...utils.cfscrape import CloudflareCaptchaError
+
+# For colorama in
+# sys.stdout = io.TextIOWrapper(sys.stdout.detach(),
+#                               encoding=sys.stdout.encoding,
+#                               errors='ignore',
+#                               line_buffering=True)
+
+
+class TestBot:
+    allerrors = dict()
+
+    from .test_inputs import test_user_inputs
+    from .test_inputs import allowed_failures
+
+    from .test_crawler import test_crawler
+    from .post_github import post_on_github
+
+    def start(self):
+        try:
+            randomized = sorted(
+                crawler_list.keys(),
+                key=lambda x: random()
+            )
+            for index, link in enumerate(randomized):
+                print('=' * 80)
+                print('>>>', index, ':', link)
+                print('=' * 80)
+
+                if link not in self.test_user_inputs:
+                    self.allerrors[link] = ['No test for: %s\n' % link]
+                    continue
+                # end if
+
+                for entry in self.test_user_inputs[link]:
+                    try:
+                        print('-' * 5, 'Input:', entry, '-' * 5)
+                        self.test_crawler(link, entry)
+                        print()
+                    except CloudflareCaptchaError:
+                        traceback.print_exc()
+                    except ConnectionError:
+                        traceback.print_exc()
+                    except Exception as err:
+                        traceback.print_exc()
+                        traces = traceback.format_tb(err.__traceback__)
+                        if link not in self.allerrors:
+                            self.allerrors[link] = []
+                        # end if
+                        self.allerrors[link].append(
+                            '> Input: %s\n%s\n%s' % (
+                                entry, err, ''.join(traces))
+                        )
+                    # end try
+                # end for
+                print('\n')
+            # end for
+            exit(0)
+        except Exception:
+            traceback.print_exc()
+        finally:
+            error_count = len([
+                x for x in self.allerrors.keys()
+                if x in self.test_user_inputs and x not in self.allowed_failures
+            ])
+            if error_count > 0:
+                message = self.build_message()
+                self.post_on_github(message)
+            # end if
+        # end if
+        # end try
+    # end def
+
+    def build_message(self):
+        num = 0
+        errors = '=' * 80 + '\n'
+        message = 'Fix %d sources:\n' % (len(self.allerrors.keys()))
+        for key in sorted(self.allerrors.keys()):
+            if key in self.allowed_failures:
+                message += '- [x] %s (allowed)\n' % key
+            elif key not in self.test_user_inputs:
+                message += '- [ ] %s (no tests)\n' % key
+            else:
+                message += '- [ ] %s\n' % key
+            # end if
+
+            for err in set(self.allerrors[key]):
+                num += 1
+                errors += '-' * 80 + '\n'
+                errors += 'Error #%d: %s\n' % (num, key)
+                errors += '-' * 80 + '\n'
+                errors += err + '\n'
+            # end for
+        # end for
+        errors += '\n' + '=' * 80 + '\n'
+        message += '-' * 80 + '\n\n'
+        return '\n%s```%s```\n' % (message, errors)
+    # end_def
+# end class
---
+++ src/bots/test/test_inputs.py
@@ -0,0 +1,232 @@
+test_user_inputs = {
+    'http://gravitytales.com/': [
+        'http://gravitytales.com/posts/novel/a-dragons-curiosity'
+    ],
+    'http://novelfull.com/': [
+        'http://novelfull.com/hidden-marriage.html',
+        'hidden',
+    ],
+    'http://www.machinenoveltranslation.com/': [
+        'http://www.machinenoveltranslation.com/a-thought-through-eternity',
+    ],
+    'http://zenithnovels.com/': [
+        'http://zenithnovels.com/infinity-armament/',
+    ],
+    'https://anythingnovel.com/': [
+        'https://anythingnovel.com/novel/king-of-gods/',
+    ],
+    'https://boxnovel.com/': [
+        'https://boxnovel.com/novel/the-rest-of-my-life-is-for-you/',
+        'cultivation chat',
+    ],
+    'https://comrademao.com/': [
+        'https://comrademao.com/novel/against-the-gods/',
+    ],
+    'https://crescentmoon.blog/': [
+        'https://crescentmoon.blog/dark-blue-and-moonlight/',
+    ],
+    'https://litnet.com/': [
+        'https://litnet.com/en/book/candy-lips-1-b106232',
+        'candy lips',
+    ],
+    'https://lnmtl.com/': [
+        'https://lnmtl.com/novel/the-strongest-dan-god',
+    ],
+    'https://m.chinesefantasynovels.com/': [
+        'https://m.chinesefantasynovels.com/3838/',
+    ],
+    'https://m.novelspread.com/': [
+        'https://m.novelspread.com/novel/the-legend-of-the-concubine-s-daughter-minglan',
+    ],
+    'https://m.romanticlovebooks.com/': [
+        'https://m.romanticlovebooks.com/xuanhuan/207.html',
+    ],
+    'https://m.wuxiaworld.co/': [
+        'https://m.wuxiaworld.co/Reincarnation-Of-The-Strongest-Sword-God/',
+        'sword',
+    ],
+    'https://meionovel.id/': [
+        'https://meionovel.id/novel/the-legendary-mechanic/',
+    ],
+    'https://mtled-novels.com/': [
+        'https://mtled-novels.com/novels/martial-god-asura/',
+        'martial'
+    ],
+    'https://bestlightnovel.com/': [
+        'https://bestlightnovel.com/novel_888103800',
+        'martial'
+    ],
+    'https://novelplanet.com/': [
+        'https://novelplanet.com/Novel/Returning-from-the-Immortal-World',
+        'immortal'
+    ],
+    'https://www.volarenovels.com/': [
+        'https://www.volarenovels.com/novel/adorable-creature-attacks',
+    ],
+    'https://webnovel.online/': [
+        'https://webnovel.online/full-marks-hidden-marriage-pick-up-a-son-get-a-free-husband',
+    ],
+    'https://www.idqidian.us/': [
+        'https://www.idqidian.us/novel/peerless-martial-god/'
+    ],
+    'https://www.novelall.com/': [
+        'https://www.novelall.com/novel/Virtual-World-Close-Combat-Mage.html',
+        'combat'
+    ],
+    'https://www.novelspread.com/': [
+        'https://www.novelspread.com/novel/the-legend-of-the-concubine-s-daughter-minglan'
+    ],
+    'https://www.readlightnovel.org/': [
+        'https://www.readlightnovel.org/top-furious-doctor-soldier'
+    ],
+    'https://www.romanticlovebooks.com/': [
+        'https://www.romanticlovebooks.com/xianxia/251.html'
+    ],
+    'https://www.royalroad.com/': [
+        'https://www.royalroad.com/fiction/21220/mother-of-learning',
+        'mother'
+    ],
+    'https://www.scribblehub.com/': [
+        'https://www.scribblehub.com/series/10442/world-keeper/',
+        'cultivation'
+    ],
+    'https://www.webnovel.com/': [
+        'https://www.webnovel.com/book/8212987205006305/Trial-Marriage-Husband%3A-Need-to-Work-Hard',
+        'martial',
+    ],
+    'https://www.worldnovel.online/': [
+        'https://www.worldnovel.online/novel/solo-leveling/',
+        'cultivation'
+    ],
+    'https://www.wuxiaworld.co/': [
+        'https://www.wuxiaworld.co/Reincarnation-Of-The-Strongest-Sword-God/',
+        'sword'
+    ],
+    'https://www.wuxiaworld.com/': [
+        'https://www.wuxiaworld.com/novel/martial-god-asura',
+        'martial',
+    ],
+    'https://creativenovels.com/': [
+        'https://creativenovels.com/novel/eternal-reverence/',
+    ],
+    'https://www.tapread.com/': [
+        'https://www.tapread.com/book/detail/80',
+    ],
+    'http://www.tapread.com/': [
+        'http://www.tapread.com/book/detail/80',
+    ],
+    'https://readnovelfull.com/': [
+        'https://readnovelfull.com/lord-of-all-realms.html',
+        'cultivation'
+    ],
+    'https://myoniyonitranslations.com/': [
+        'https://myoniyonitranslations.com/top-management/',
+        'https://myoniyonitranslations.com/category/god-of-tennis',
+    ],
+    'https://babelnovel.com/': [
+        'https://babelnovel.com/books/against-the-gods',
+        'martial god AsurA'
+    ],
+    'https://wuxiaworld.online/': [
+        'https://wuxiaworld.online/trial-marriage-husband-need-to-work-hard',
+        'cultivation',
+    ],
+    'https://www.novelv.com/': [
+        'https://www.novelv.com/0/349/'
+    ],
+    'http://fullnovel.live/': [
+        'http://fullnovel.live/novel-a-will-eternal',
+        'will eternal',
+    ],
+    'https://www.noveluniverse.com/': [
+        'https://www.noveluniverse.com/index/novel/info/id/15.html'
+    ],
+    'https://novelraw.blogspot.com/': [
+        'https://novelraw.blogspot.com/2019/03/dragon-king-son-in-law-mtl.html'
+    ],
+    'https://light-novel.online/': [
+        'https://light-novel.online/great-tyrannical-deity',
+        'tyrannical'
+    ],
+    'https://www.rebirth.online/': [
+        'https://www.rebirth.online/novel/upside-down'
+    ],
+    'https://www.jieruihao.cn/': [
+        'https://www.jieruihao.cn/novel/against-the-gods/',
+    ],
+    'https://www.wattpad.com/': [
+        'https://www.wattpad.com/story/87505567-loving-mr-jerkface-%E2%9C%94%EF%B8%8F'
+    ],
+    'https://novelgo.id/': [
+        'https://novelgo.id/novel/the-mightiest-leveling-system/'
+    ],
+    'https://yukinovel.me/': [
+        'https://yukinovel.me/novel/the-second-coming-of-avarice/',
+    ],
+    'https://www.asianhobbyist.com/': [
+        'https://www.asianhobbyist.com/series/that-time-i-got-reincarnated-as-a-slime/'
+    ],
+    'https://kisslightnovels.info/': [
+        'https://kisslightnovels.info/novel/solo-leveling/'
+    ],
+    'https://novelonlinefull.com/': [
+        'https://novelonlinefull.com/novel/abo1520855001564322110'
+    ],
+    'https://www.machine-translation.org/': [
+        'https://www.machine-translation.org/novel/bace21c9b10d34e9/world-of-cultivation.html'
+    ],
+    'https://www.fanfiction.net/': [
+        'https://www.fanfiction.net/s/7268451/1/Facebook-For-wizards'
+    ],
+    'https://www.mtlnovel.com/': [
+        'https://www.mtlnovel.com/trapped-in-a-typical-idol-drama/'
+    ],
+    'https://wordexcerpt.com/': [
+        'https://wordexcerpt.com/series/transmigration-raising-the-child-of-the-male-lead-boss/'
+    ],
+    'https://www.translateindo.com/': [
+        'https://www.translateindo.com/demon-wang-golden-status-favoured-fei/'
+    ],
+    'https://ranobelib.me/': [
+        'https://ranobelib.me/solo-leveling'
+    ],
+    'https://novelringan.com/': [
+        'https://novelringan.com/series/the-most-loving-marriage-in-history-master-mus-pampered-wife/'
+    ],
+    'https://wuxiaworld.site/': [
+        'https://wuxiaworld.site/novel/only-i-level-up/'
+    ],
+    'https://id.mtlnovel.com/': [
+        'https://id.mtlnovel.com/the-strongest-plane-becomes-god/'
+    ],
+    'https://www.shinsori.com/': [
+        'https://www.shinsori.com/akuyaku-reijou-ni-nanka-narimasen/'
+    ],
+    'https://www.flying-lines.com/': [
+        'https://www.flying-lines.com/novel/one-useless-rebirth'
+    ],
+    'https://book.qidian.com/': [
+        'https://book.qidian.com/info/1010734492'
+    ],
+    'https://kiss-novel.com/': [
+        'https://kiss-novel.com/the-invincible-dragon-emperor'
+    ],
+    'https://www.machine-translation.org/': [
+        'https://www.machine-translation.org/novel/a5eee127d75da0d2/long-live-summons.html'
+    ],
+    'https://www.aixdzs.com/': [
+        'https://www.aixdzs.com/d/66/66746/'
+    ],
+    'https://webnovelonline.com/': [
+        'https://webnovelonline.com/novel/the_anarchic_consort'
+    ],
+}
+
+allowed_failures = [
+    # 'https://ranobelib.me/',
+    # 'https://m.chinesefantasynovels.com/',
+    # 'https://m.romanticlovebooks.com/',
+    # 'https://www.romanticlovebooks.com/',
+    # 'https://readnovelfull.com/',
+    # 'https://book.qidian.com/'
+]
