1312748b0c038c8abc7ea2f2ae56d893778c3857
Sudipto Chandra <dipu.sudipta@gmail.com>
2019-12-24 12:16:12 +0600

    [qidiancom] fix: get volume list from ajax call

--- src/spiders/qidiancom.py
+++ src/spiders/qidiancom.py
@@ -5,6 +5,8 @@ from ..utils.crawler import Crawler
 
 logger = logging.getLogger('QIDIAN_COM')
 
+chapter_list_url = 'https://book.qidian.com/ajax/book/category?_csrfToken=%s&bookId=%s'
+chapter_details_url = 'https://read.qidian.com/chapter/%s'
 
 class QidianComCrawler(Crawler):
     def initialize(self):
@@ -15,34 +17,41 @@ class QidianComCrawler(Crawler):
         '''Get novel title, autor, cover etc'''
         logger.debug('Visiting %s', self.novel_url)
         soup = self.get_soup(self.novel_url)
-
+        
         self.novel_title = soup.select_one('.book-info h1 em').text
         logger.info('Novel title: %s', self.novel_title)
 
         self.novel_author = soup.select_one('.book-info h1 a.writer').text
         logger.info('Novel author: %s', self.novel_author)
 
-        self.novel_cover = self.absolute_url(
-            soup.select_one('#bookImg img')['src'])
-        self.novel_url = '/'.join(self.novel_url.split('/')[:-1])
+        book_img = soup.select_one('#bookImg')
+        self.novel_cover = self.absolute_url(book_img.find('img')['src'])
+        self.novel_cover = '/'.join(self.novel_cover.split('/')[:-1])
         logger.info('Novel cover: %s', self.novel_cover)
 
-        for volume in soup.select('.volume-wrap .volume'):
-            vol_title = volume.select_one('h3')
-            for tag in vol_title.select('a.subscri, span.free, em.count'):
-                tag.decompose()
-            # end for
+        self.book_id = book_img['data-bid']
+        logger.debug('Book Id: %s', self.book_id)
+
+        self.csrf = self.cookies['_csrfToken']
+        logger.debug('CSRF Token: %s', self.csrf)
+
+        volume_url = chapter_list_url % (self.csrf, self.book_id)
+        logger.debug('Visiting %s', volume_url)
+        data = self.get_json(volume_url)
+
+        for volume in data['data']['vs']:
+            vol_id = len(self.volumes) + 1
             self.volumes.append({
-                'id': len(self.volumes) + 1,
-                'title': vol_title.text.strip(),
+                'id': vol_id,
+                'title': volume['vN'],
             })
-            for li in volume.select('li'):
-                a = li.select_one('a')
+            for chapter in volume['cs']:
+                ch_id = len(self.chapters) + 1
                 self.chapters.append({
-                    'id': int(li['data-rid']),
-                    'volume': len(self.volumes),
-                    'title': a.text.strip(),
-                    'url': self.absolute_url(a['href']),
+                    'id': ch_id,
+                    'volume': vol_id,
+                    'title': chapter['cN'],
+                    'url': chapter_details_url % chapter['cU'],
                 })
             # end for
         # end for
