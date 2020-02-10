f04cc9979c05c49a7f7b90d33cfa079efcbe6d3a
Sudipto Chandra <dipu.sudipta@gmail.com>
2020-02-10 18:21:05 +0600

    [v3] Improve argument parsing and config manager

--- src_v3/app/arguments.py
+++ src_v3/app/arguments.py
@@ -1,14 +1,56 @@
 # -*- coding: utf-8 -*-
-from ..assets import get_version
-from .utility.argument import Namespace, ArgumentBuilder as Arg
+from argparse import ArgumentParser, Namespace
 
-_builder = Arg(group=[
-    Arg('-v', '--version', action='version',
-        version='Lightnovel Crawler ' + get_version()),
-    Arg('--config', dest='config', metavar='CONFIG_FILE',
-        help='The config file path'),
-])
+from ..assets import get_version
+
+_parser = ArgumentParser(
+    epilog='~' * 78,
+    # usage="lnc [options...]\n"
+    # "       lncrawl [options...]\n"
+    # "       lightnovel-crawler [options...]"
+)
+
+
+# Use a dictionary to add new argument
+# Use tuple for mutually exclusive group
+# Use list for a simple group
+_arguments = [
+    dict(flags=('-v', '--version'), action='version',
+         version='Lightnovel Crawler ' + get_version()),
+    dict(flags=('--config'), dest='config', metavar='CONFIG_FILE',
+         help='The config file path'),
+    dict(flags=('method'), nargs='?', default='app',
+         choices=['app', 'bot', 'configure'],
+         help='The method name (default: app)'),
+]
+
+
+def _build(args: list = None, parser: ArgumentParser = None):
+    if args is None:
+        args: list = globals().get('_arguments')
+    if parser is None:
+        parser: ArgumentParser = globals().get('_parser')
+    for arg in args:
+        if isinstance(arg, dict):
+            flags = arg.pop('flags', tuple())
+            if isinstance(flags, tuple):
+                flags = list(flags)
+            elif not isinstance(flags, list):
+                flags = [flags]
+            parser.add_argument(*flags, **arg)
+        elif isinstance(arg, list):
+            group = parser.add_argument_group()
+            _build(arg, group)
+        elif isinstance(arg, tuple):
+            mutex = parser.add_mutually_exclusive_group()
+            _build(arg, mutex)
+        else:
+            raise ValueError(
+                f"Expected list, tuple or dict. Found {type(arg)}")
 
 
 def get_args() -> Namespace:
-    return _builder.parse()
+    if not globals().get('_parsed'):
+        _build()
+        globals()['_parsed'] = _parser.parse_args()
+    return globals().get('_parsed')
--- src_v3/app/config.py
+++ src_v3/app/config.py
@@ -15,35 +15,30 @@ from .arguments import get_args
 class Config(metaclass=Singleton):
     '''Access and manage app configurations'''
 
-    def __init__(self):
+    def __init__(self) -> None:
         # Load default configuration
-        self._config: Mapping[str, Any] = dict(DEFAULT_CONFIG)
-
-        # Resolve config file path
-        args = get_args()
-        self._config_file = args.config or str(
-            self.defaults.work_directory / 'config.json')
+        self.config = dict(DEFAULT_CONFIG)
 
         # Load configuration from file
         self.load()
 
         # Initialize logger
-        logging.config.dictConfig(self.logging.__data__)
+        logging.config.dictConfig(self.logging.section)
         logging.info("Log initialized")
 
     def load(self) -> None:
         '''Loads config from a file'''
-        if not os.path.isfile(self._config_file):
+        if not os.path.isfile(self.config_file):
             return
-        with open(self._config_file, mode='r', encoding='utf-8') as fp:
-            config = json.load(fp)
-            self._config.update(config)
+        with open(self.config_file, mode='r', encoding='utf-8') as fp:
+            config: dict = json.load(fp)
+            self.config.update(config)
 
     def save(self) -> None:
         '''Saves config to a file'''
-        os.makedirs(os.path.dirname(self._config_file), exist_ok=True)
-        with open(self._config_file, mode='w', encoding='utf-8') as fp:
-            json.dump(self._config, fp, indent=2)
+        os.makedirs(os.path.dirname(self.config_file), exist_ok=True)
+        with open(self.config_file, mode='w', encoding='utf-8') as fp:
+            json.dump(self.config, fp, indent=2)
 
     # ========================================================================= #
     # Property declarations for easy access                                     #
@@ -51,29 +46,43 @@ class Config(metaclass=Singleton):
 
     class __Section__(metaclass=Singleton):
         def __init__(self, data: Mapping[str, Any]):
-            setattr(self, '__data__', data)
+            setattr(self, 'section', data)
 
     class _Defaults(__Section__):
         @property
         def work_directory(self) -> PurePath:
             fallback = str(Path(os.getcwd()) / 'Lightnovels')
-            return Path(self.__data__.get('work_directory', fallback))
+            return Path(self.section.get('work_directory', fallback))
 
     class _Logging(__Section__):
         @property
+        def loggers(self) -> dict:
+            return self.section.get('loggers', {})
+
+        @property
+        def handlers(self) -> dict:
+            return self.section.get('handlers', {})
+
+        @property
         def root_log_level(self) -> str:
-            loggers = self.__data__.get('loggers', {})
-            return loggers.get('', {}).get('level', 'NOTSET')
+            return self.loggers.get('', {}).get('level', 'NOTSET')
 
         @property
         def root_log_handlers(self) -> List[str]:
-            loggers = self.__data__.get('loggers', {})
-            return loggers.get('', {}).get('handlers', [])
+            return self.loggers.get('', {}).get('handlers', [])
 
     @property
     def defaults(self) -> Optional[_Defaults]:
-        return self._Defaults(self._config['defaults'])
+        return self._Defaults(self.config['defaults'])
 
     @property
     def logging(self) -> Optional[_Logging]:
-        return self._Logging(self._config['logging'])
+        return self._Logging(self.config['logging'])
+
+    @property
+    def config_file(self) -> str:
+        if not hasattr(self, '_config_file_'):
+            fname = str(self.defaults.work_directory / 'config.json')
+            fname = get_args().config or fname
+            setattr(self, '_config_file_', fname)
+        return getattr(self, '_config_file_', '')
