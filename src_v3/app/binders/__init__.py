82352056ff20378d3d8b7fa9e6fbe4832b69c79b
Sudipto Chandra <dipu.sudipta@gmail.com>
2020-02-12 16:55:09 +0600

    [v3] Add binder's output format enum

---
+++ src_v3/app/binders/__init__.py
@@ -0,0 +1,1 @@
+from .output_format import OutputFormat
---
+++ src_v3/app/binders/output_format.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from ...meta import StrEnum
+
+
+class OutputFormat(StrEnum):
+    CHM = 'chm'
+    AZW3 = 'azw3'
+    DOCX = 'docx'
+    EPUB = 'epub'
+    FB2 = 'fb2'
+    HTML = 'html'
+    JSON = 'json'
+    LIT = 'lit'
+    LRF = 'lrf'
+    MOBI = 'mobi'
+    OEB = 'oeb'
+    PDB = 'pdb'
+    PDF = 'pdf'
+    PML = 'pml'
+    RB = 'rb'
+    RTF = 'rtf'
+    SNB = 'snb'
+    TCR = 'tcr'
+    TXT = 'txt'
+    WEB = 'web'
