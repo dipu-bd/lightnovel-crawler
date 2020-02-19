72149c351f943cb36383c74c065fe709ed2844d9
Sudipto Chandra <dipu.sudipta@gmail.com>
2020-02-19 17:20:39 +0600

    [v3] Use CONFIG in browser

--- src/app/browser/browser.py
+++ src/app/browser/browser.py
@@ -5,44 +5,42 @@ import tempfile
 from concurrent.futures import ThreadPoolExecutor
 from typing import *
 
-import cloudscraper
+from cloudscraper import CloudScraper, create_scraper
 
 from ..utility import reformat_url
 from .response import BrowserResponse
 
-JsonType = Mapping[AnyStr, Any]
+from .. import CONFIG
 
 
-class Browser:
+class Browser(object):
+    def __new__(cls, *args, **kwargs):
+        instance = super(Browser, cls).__new__(cls, *args, **kwargs)
+        atexit.register(instance.__exit__)
+        return instance
+
     def __init__(self, workers: int = 5):
-        # Register cleaner
-        atexit.register(self.close)
-
         # Initialize class variables
         self.executor: ThreadPoolExecutor = ThreadPoolExecutor(workers)
 
         # Initialize internal client
-        self.client: cloudscraper.CloudScraper = cloudscraper.create_scraper(
-            # Docs: https://github.com/VeNoMouS/cloudscraper
-            allow_brotli=False,
-            browser=dict(
-                browser='firefox',
-                desktop=True,
-                mobile=False,
-            ),
-        )
+        cs_config = CONFIG.browser.cloudscraper
+        self.client: CloudScraper = create_scraper(**cs_config)
 
-    def close(self):
-        self.client.close()
-        self.executor.shutdown(True)
+    def __exit__(self):
+        if hasattr(self, 'client'):
+            self.client.close()
+        if hasattr(self, 'executor'):
+            self.executor.shutdown(True)
 
     @property
     def cookies(self) -> Mapping[Any, Any]:
         return self.client.cookies.get_dict()
 
     def get(self, url: str, **kwargs) -> BrowserResponse:
+        timeout = kwargs.get('timeout', None)
         future = self.executor.submit(self.client.get, url, **kwargs)
-        return BrowserResponse(future)
+        return BrowserResponse(future, timeout)
 
     def post(self, url, body: Mapping[str, Any] = None, multipart: bool = False, **kwargs) -> BrowserResponse:
         if not body:
@@ -56,18 +54,19 @@ class Browser:
 
         kwargs['data'] = body
         kwargs['headers'] = headers
+        timeout = kwargs.get('timeout', None)
         future = self.executor.submit(self.client.post, url, **kwargs)
-        return BrowserResponse(future)
+        return BrowserResponse(future, timeout)
 
-    def download_file(self, url: str, filepath: str = None, **kwargs) -> str:
+    def download(self, url: str, filepath: str = None, **kwargs) -> str:
         if filepath is None:
             _, filepath = tempfile.mkstemp()
-
-        chunk_size = 10 * 1024  # TODO: get from Config()
 
         kwargs['stream'] = True
         res = self.get(url, **kwargs)
         raw = res.raw()
+
+        chunk_size = CONFIG.browser.stream_chunk_size
 
         with open(filepath, 'wb') as f:
             while True:
--- src/app/browser/response.py
+++ src/app/browser/response.py
@@ -2,52 +2,69 @@
 
 import atexit
 from concurrent.futures import Future
+from threading import Lock
+from typing import Union
 
 from bs4 import BeautifulSoup
 from requests import Response
 from urllib3 import HTTPResponse
 
+from .. import CONFIG
+
 
 class BrowserResponse:
 
-    def __init__(self, furure: Future):
-        atexit.register(self.close)
-        self.count = 0
-        self.total = 0
-        self._future = furure
+    def __init__(self, future: Future, timeout: Union[int, float] = 0):
+        self.__lock = Lock()
+        self.__resolve(future, timeout)
 
-    def close(self):
-        if hasattr(self, '_closed'):
-            return
-        setattr(self, '_closed', True)
-        if hasattr(self, '_resp'):
-            getattr(self, '_resp').close()
+    def __resolve(self, future: Future, timeout: Union[int, float]):
+        if timeout is not None and timeout <= 0:
+            timeout = CONFIG.browser.response_timeout
+        try:
+            self.__lock.acquire()
+            resp: Response = future.result(timeout)
+            resp.raise_for_status()
+            self._resp = resp
+            atexit.register(self._resp.close)
+        except Exception as e:
+            self._error = e
+        finally:
+            self.__lock.release()
 
-    def response(self, timeout: float = None) -> Response:
-        # Return from cache if available
-        if hasattr(self, '_resp'):
-            return getattr(self, '_resp')
-        # Wait for future to finish
-        resp: Response = self._future.result(timeout)
-        resp.raise_for_status()
-        # Update cache
-        setattr(self, '_resp', resp)
-        return resp
+    @property
+    def response(self) -> Response:
+        with self.__lock:
+            if hasattr(self, '_resp'):
+                return self._resp
+            elif hasattr(self, '_error'):
+                raise self._error
+            else:
+                raise ValueError('No response or error')
 
+    @property
+    def encoding(self):
+        return self.response.apparent_encoding
+
+    @property
     def raw(self) -> HTTPResponse:
-        return self.response().raw
+        return self.response.raw
 
+    @property
     def content(self) -> bytes:
-        return self.response().content
+        return self.response.content
 
+    @property
     def text(self) -> str:
-        return self.response().text
+        return self.response.text
 
-    def json(self, **kwargs):
-        return self.response().json(**kwargs)
+    @property
+    def json(self) -> dict:
+        return self.response.json()
 
-    def soup(self, parser: str = 'html5lib') -> BeautifulSoup:
-        # TODO: use Config() to get parser
-        html = self.content().decode(encoding='utf-8', errors='ignore')
-        soup = BeautifulSoup(markup=self.text(), features=parser)
+    @property
+    def soup(self) -> BeautifulSoup:
+        parser: str = CONFIG.browser.soup_parser
+        # html = self.content().decode(encoding='utf-8', errors='ignore')
+        soup = BeautifulSoup(markup=self.text, features=parser)
         return soup
