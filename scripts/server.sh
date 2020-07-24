05993b10b4f3764a5e0c8374e5394b22fa864dc1
Sudipto Chandra <dipu.sudipta@gmail.com>
2020-07-24 06:13:31 +0600

    Create server.sh

---
+++ scripts/server.sh
@@ -0,0 +1,24 @@
+export LC_ALL="en_US.UTF-8"
+
+git pull origin master
+
+if [ ! -d venv ]; then
+    python -m venv venv
+fi
+
+. venv/bin/activate
+pip install -U -r requirements.txt
+pip install -U -r dev-requirements.txt
+
+if [ "$1" = "stop" ]; then
+    exit 0
+fi
+
+echo "Stopping previous instances..."
+pgrep python -a | grep "discord" | awk '{print $1}' | xargs kill -9 || true
+
+echo "Starting new instances..."
+nohup python3 . --bot discord --shard-id 0 --shard-count 2 > /dev/null 2>&1 &
+nohup python3 . --bot discord --shard-id 1 --shard-count 2 > /dev/null 2>&1 &
+
+echo "Done."
