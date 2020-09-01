94d43b2a4221f236b6815a74efe73aaba66f4aa6
Sudipto Chandra <dipu.sudipta@gmail.com>
2020-09-01 12:48:07 +0600

    [listnovel] Fix chapter list

--- lncrawl/sources/listnovel.py
+++ lncrawl/sources/listnovel.py
@@ -8,6 +8,9 @@ from ..utils.crawler import Crawler
 logger = logging.getLogger('LIST_NOVEL')
 
 
+chapter_list_url = 'https://listnovel.com/wp-admin/admin-ajax.php'
+
+
 class ListNovelCrawler(Crawler):
     base_url = 'https://listnovel.com/'
 
@@ -23,7 +26,8 @@ class ListNovelCrawler(Crawler):
         self.novel_title = possible_title.text.strip()
         logger.info('Novel title: %s', self.novel_title)
 
-        self.novel_cover = self.absolute_url(soup.select_one('.summary_image a img')['data-src'])
+        self.novel_cover = self.absolute_url(
+            soup.select_one('.summary_image a img')['data-src'])
         logger.info('Novel cover: %s', self.novel_cover)
 
         self.novel_author = ' '.join([
@@ -32,7 +36,13 @@ class ListNovelCrawler(Crawler):
         ])
         logger.info('%s', self.novel_author)
 
-        for a in reversed(soup.select('.main-col li.wp-manga-chapter a')):
+        self.novel_id = soup.select_one('#manga-chapters-holder')['data-id']
+        logger.info('Novel id: %s', self.novel_id)
+
+        response = self.submit_form(
+            chapter_list_url, data='action=manga_get_chapters&manga=' + self.novel_id)
+        soup = self.make_soup(response)
+        for a in reversed(soup.select('.wp-manga-chapter a')):
             chap_id = len(self.chapters) + 1
             vol_id = 1 + len(self.chapters) // 100
             if chap_id % 100 == 1:
