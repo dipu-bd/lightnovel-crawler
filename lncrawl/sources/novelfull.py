0d3d8b1793abf8846f6766510f88418c3c117833
SirGryphin <bballuk@gmail.com>
2020-10-01 22:25:35 +0100

    minor fixes

--- lncrawl/sources/novelfull.py
+++ lncrawl/sources/novelfull.py
@@ -3,6 +3,7 @@ import re
 import logging
 from concurrent import futures
 from ..utils.crawler import Crawler
+from bs4 import Comment
 
 logger = logging.getLogger(__name__)
 search_url = 'https://novelfull.com/search?keyword=%s'
@@ -122,14 +123,33 @@ class NovelFullCrawler(Crawler):
         '''Download body of a single chapter and return as clean html format.'''
         logger.info('Downloading %s', chapter['url'])
         soup = self.get_soup(chapter['url'])
+
         content = soup.select_one('div#chapter-content')
+
+        # Removes junk text from chapters.
+        self.blacklist_patterns = [
+            r'^Translator:',
+            r'^Editor:',
+            r'^Atlas Studios',
+            r'Read more chapter on NovelFull'
+            r'^full thich ung',
+        ]
+
+        # Some comments in source code of site gets converted into text/paragraphs. This removes it.
+        for comment in soup.findAll(text=lambda text:isinstance(text, Comment)):
+            comment.extract()
+
+        self.clean_contents(content)
+
         for ads in content.findAll('div', {"align": 'left'}):
             ads.decompose()
         for ads in content.findAll('div', {"align": 'center'}):
             ads.decompose()
-        for ads in content.select('.adsbygoogle, script, ins, .ads, .ads-holder'):
-            ads.decompose
-        # self.clean_contents(content)
-        return str(content)
+        for ads in content.select('h3, .adsbygoogle, script, ins, .ads, .ads-holder'):
+            ads.decompose()
+        # return str(content)
+        # Changed so excess div tags are removed and all chapters text is in p tag, so its better formatted. Also added h3 tag to above decompose to remove double chapter headings.
+        body = self.extract_contents(content)
+        return '<p>' + '</p><p>'.join(body) + '</p>'
     # end def
 # end class
