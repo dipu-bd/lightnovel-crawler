66597be26317168dbcdd549746c87bde57fd5e39
SirGryphin <bballuk@gmail.com>
2020-12-25 08:14:19 +0000

    problem with commit

--- lncrawl/sources/88tang.py
+++ lncrawl/sources/88tang.py
@@ -5,7 +5,7 @@ import re
 
 from ..utils.crawler import Crawler
 
-logger = logging.getLogger('88TANG')
+logger = logging.getLogger('88_TANG')
 
 class TangEatDrinkRead(Crawler):
     base_url = 'https://88tangeatdrinkread.wordpress.com/'
