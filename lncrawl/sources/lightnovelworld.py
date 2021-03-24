4a12c2605d569d8c25e93c423134705aa19af08b
SirGryphin <bballuk@gmail.com>
2021-03-24 12:55:26 +0000

    added source

---
+++ lncrawl/sources/lightnovelworld.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import json
+import logging
+import re
+from ..utils.crawler import Crawler
+
+logger = logging.getLogger(__name__)
+
+
+class LightNovelWorldCrawler(Crawler):
+    base_url = 'https://lightnovel.world/'
+
+    def read_novel_info(self):
+        '''Get novel title, autor, cover etc'''
+        logger.debug('Visiting %s', self.novel_url)
+        soup = self.get_soup(self.novel_url)
+
+        possible_title = soup.select_one('li.text1')
+        for span in possible_title.select('span'):
+            span.extract()
+        # end for
+        self.novel_title = possible_title.text.strip()
+        logger.info('Novel title: %s', self.novel_title)
+
+        self.novel_cover = self.absolute_url(
+            soup.select_one('.book_info_l img')['src'])
+        logger.info('Novel cover: %s', self.novel_cover)
+
+        # NOTE: Could not get author name from page, kept getting ResultSet object has no attribute 'text'.
+        # self.novel_author = soup.select('h1 span').text.strip()
+        # logger.info('Novel author: %s', self.novel_author)
+
+        for a in soup.select('div#chapter_content ul li a'):
+            chap_id = len(self.chapters) + 1
+            if len(self.chapters) % 100 == 0:
+                vol_id = chap_id//100 + 1
+                vol_title = 'Volume ' + str(vol_id)
+                self.volumes.append({
+                    'id': vol_id,
+                    'title': vol_title,
+                })
+            # end if
+            self.chapters.append({
+                'id': chap_id,
+                'volume': vol_id,
+                'url':  self.absolute_url(a['href']),
+                'title': a.text.strip() or ('Chapter %d' % chap_id),
+            })
+        # end for
+    # end def
+
+    def download_chapter_body(self, chapter):
+        '''Download body of a single chapter and return as clean html format.'''
+        logger.info('Downloading %s', chapter['url'])
+        soup = self.get_soup(chapter['url'])
+
+        contents = soup.select_one('div#content_detail')
+        for ads in contents.select("div"):
+            ads.decompose()
+
+        return str(contents)
+
+    # end def
+# end class
