80e8e3cdd5cac8f883ee0d1665e317aead801701
Sudipto Chandra <dipu.sudipta@gmail.com>
2021-05-03 09:40:50 +0600

    Add search in lightnovelbastion #602

--- lncrawl/sources/lightnovelbastion.py
+++ lncrawl/sources/lightnovelbastion.py
@@ -7,27 +7,30 @@ from urllib.parse import quote_plus
 from ..utils.crawler import Crawler
 
 logger = logging.getLogger(__name__)
-search_url = 'https://lightnovelbastion.com/?s=%s'
-chapter_list_url = 'https://lightnovelbastion.com/wp-admin/admin-ajax.php'
+search_url = 'https://lightnovelbastion.com/?s=%s&post_type=wp-manga&author=&artist=&release='
 
 
 class LightNovelBastion(Crawler):
     base_url = 'https://lightnovelbastion.com/'
 
-    # def search_novel(self, query):
-    #     query = quote_plus(query.lower())
-    #     soup = self.get_soup(search_url % query)
+    def search_novel(self, query):
+        query = quote_plus(query.lower())
+        soup = self.get_soup(search_url % query)
 
-    #     results = []
-    #     for a in soup.select('#loop-content .post-title a'):
-    #         results.append({
-    #             'title': a['title'].strip(),
-    #             'url': self.absolute_url(a['href']),
-    #         })
-    #     # end for
+        results = []
+        for tab in soup.select('.c-tabs-item__content')[:20]:
+            a = tab.select_one('.post-title h4 a')
+            latest = tab.select_one('.latest-chap .chapter a').text
+            votes = tab.select_one('.rating .total_votes').text
+            results.append({
+                'title': a.text.strip(),
+                'url': self.absolute_url(a['href']),
+                'info': '%s | Rating: %s' % (latest, votes),
+            })
+        # end for
 
-    #     return results
-    # # end def
+        return results
+    # end def
 
     def initialize(self):
         self.executor = ThreadPoolExecutor(1)
@@ -37,24 +40,22 @@ class LightNovelBastion(Crawler):
         logger.debug('Visiting %s', self.novel_url)
         soup = self.get_soup(self.novel_url)
 
-        self.novel_title = ' '.join([
-            str(x)
-            for x in soup.select_one('.post-title').select_one('h1, h2, h3, h4').contents
-            if not x.name
-        ]).strip()
+        possible_title = soup.select_one('.post-title h3')
+        for span in possible_title.select('span'):
+            span.extract()
+        # end for
+        self.novel_title = possible_title.text.strip()
         logger.info('Novel title: %s', self.novel_title)
 
-        probable_img = soup.select_one('.summary_image img')
-        if probable_img:
-            self.novel_cover = self.absolute_url(probable_img['data-src'])
+        self.novel_cover = self.absolute_url(
+            soup.select_one('.summary_image a img')['data-src'])
         logger.info('Novel cover: %s', self.novel_cover)
 
-        author = soup.select('.author-content a')
-        if len(author) == 2:
-            self.novel_author = author[0].text + ' (' + author[1].text + ')'
-        else:
-            self.novel_author = author[0].text
-        logger.info('Novel author: %s', self.novel_author)
+        self.novel_author = ' '.join([
+            a.text.strip()
+            for a in soup.select('.author-content a[href*="novel-author"]')
+        ])
+        logger.info('%s', self.novel_author)
 
         if len(soup.select('ul.version-chap li.has-child li.wp-manga-chapter')):
             logger.debug('Has volume definitions')
@@ -98,8 +99,19 @@ class LightNovelBastion(Crawler):
         logger.info('Visiting %s', chapter['url'])
         soup = self.get_soup(chapter['url'])
         contents = soup.select_one('.reading-content .text-left')
-        for bad in contents.select('.lnbad-tag, blockquote'):
+
+        bad_css = ','.join([
+            'h3',
+            '.lnbad-tag',
+            'blockquote',
+            '.code-block',
+            'script',
+            '.adsbygoogle',
+            '.sharedaddy',
+        ])
+        for bad in contents.select(bad_css):
             bad.decompose()
+
         body = self.extract_contents(contents)
         return '<p>' + '</p><p>'.join(body) + '</p>'
     # end def
