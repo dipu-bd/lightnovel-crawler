63b64bb9db9c5587f97b2f8c22a575f6853206e7
SirGryphin <bballuk@gmail.com>
2021-05-12 00:54:21 +0100

    Fixed chapter list

--- lncrawl/sources/wspadancewichita.py
+++ lncrawl/sources/wspadancewichita.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
+import json
 import logging
 import re
-from concurrent import futures
 from ..utils.crawler import Crawler
+from ..utils.cleaner import cleanup_text
 
 logger = logging.getLogger(__name__)
 search_url = 'http://wspadancewichita.com/search?keyword=%s'
+full_chapter_url = 'http://wspadancewichita.com/ajax/chapter-archive?novelId=%s'
 
 
 class wspadancewichita(Crawler):
@@ -16,124 +18,84 @@ class wspadancewichita(Crawler):
         soup = self.get_soup(search_url % query)
 
         results = []
-        for tab in soup.select('.col-novel-main .list-novel .row'):
-            search_title = tab.select_one('.novel-title a')
-            latest = tab.select_one('.text-info a').text.strip()
+        for result in soup.select('div.col-novel-main div.list.list-novel div.row')[:5]:
+            url = self.absolute_url(
+                result.select_one('h3.novel-title a')['href'])
+            title = result.select_one('h3.novel-title a')['title']
+            last_chapter = result.select_one('span.chr-text').text.strip()
             results.append({
-                'title': search_title.text.strip(),
-                'url': self.absolute_url(
-                    tab.select_one('.novel-title a')['href']),
-                'info': 'Latest chapter: %s' % (latest)
+                'url': url,
+                'title': title,
+                'info': 'last chapter : %s' % last_chapter,
             })
         # end for
-
         return results
     # end def
 
     def read_novel_info(self):
         '''Get novel title, autor, cover etc'''
         logger.debug('Visiting %s', self.novel_url)
-        soup = self.get_soup(self.novel_url)
+        soup = self.get_soup(self.novel_url + '?waring=1')
 
-        self.novel_title = ' '.join([
-            str(x)
-            for x in soup.select_one('.title').contents
-            if not x.name
-        ]).strip()
+        self.novel_title = soup.select_one('h3.title').text.strip()
         logger.info('Novel title: %s', self.novel_title)
 
         self.novel_cover = self.absolute_url(
-            soup.select_one('.book img')['src'])
+            soup.select_one('div.book img')['src'])
         logger.info('Novel cover: %s', self.novel_cover)
 
-        author = soup.find_all(href=re.compile('author'))
-        if len(author) == 2:
-            self.novel_author = author[0].text + ' (' + author[1].text + ')'
-        else:
-            self.novel_author = author[0].text
+        author = []
+        for a in soup.select('ul.info.info-meta li')[1].select('a'):
+            author.append(a.text.strip())
+        # end for
+
+        self.novel_author = ", ".join(author)
+
         logger.info('Novel author: %s', self.novel_author)
 
-        # This is copied from the Novelfull pagination 'hanlder' with minor tweaks
-        pagination_links = soup.select('.pagination li a')
-        pagination_page_numbers = []
-        for pagination_link in pagination_links:
-            # Boxnovel.org pagination numbering boxes contain non-digit characters
-            if pagination_link.text.isdigit():
-                pagination_page_numbers.append(int(pagination_link.text))
+        novel_id = soup.select_one('div#rating')['data-novel-id']
 
-        page_count = max(
-            pagination_page_numbers) if pagination_page_numbers else 0
-        logger.info('Chapter list pages: %d' % page_count)
+        chapter_url = full_chapter_url % novel_id
+        logger.debug('Visiting %s', chapter_url)
 
-        logger.info('Getting chapters...')
-        futures_to_check = {
-            self.executor.submit(
-                self.download_chapter_list,
-                i + 1,
-            ): str(i)
-            for i in range(page_count + 1)
-        }
-        [x.result() for x in futures.as_completed(futures_to_check)]
-
-        # Didn't test without this, but with pagination the chapters could be in different orders
-        logger.info('Sorting chapters...')
-        self.chapters.sort(key=lambda x: x['volume'] * 1000 + x['id'])
-
-        # Copied straight from Novelfull
-        logger.info('Adding volumes...')
-        mini = self.chapters[0]['volume']
-        maxi = self.chapters[-1]['volume']
-        for i in range(mini, maxi + 1):
-            self.volumes.append({'id': i})
+        chapter_soup = self.get_soup(chapter_url)
+        chapters = chapter_soup.select('li a')
+        for a in chapters:
+            for span in a.findAll('span'):
+                span.decompose()
+            # end for
         # end for
-    # end def
 
-    def download_chapter_list(self, page):
-        '''Download list of chapters and volumes.'''
-        url = self.novel_url.split('?')[0].strip('/')
-        url += '?page=%d&per-page=50' % page
-        soup = self.get_soup(url)
-
-        for a in soup.select('ul.list-chapter li a'):
-            title = a['title'].strip()
-
-            chapter_id = len(self.chapters) + 1
-            # match = re.findall(r'ch(apter)? (\d+)', title, re.IGNORECASE)
-            # if len(match) == 1:
-            #     chapter_id = int(match[0][1])
-            # # end if
-
-            volume_id = 1 + (chapter_id - 1) // 100
-            match = re.findall(r'(book|vol|volume) (\d+)',
-                               title, re.IGNORECASE)
-            if len(match) == 1:
-                volume_id = int(match[0][1])
+        for x in chapters:
+            chap_id = len(self.chapters) + 1
+            if len(self.chapters) % 100 == 0:
+                vol_id = chap_id//100 + 1
+                vol_title = 'Volume ' + str(vol_id)
+                self.volumes.append({
+                    'id': vol_id,
+                    'title': vol_title,
+                })
             # end if
-
-            data = {
-                'title': title,
-                'id': chapter_id,
-                'volume': volume_id,
-                'url': self.absolute_url(a['href']),
-            }
-            self.chapters.append(data)
+            self.chapters.append({
+                'id': chap_id,
+                'volume': vol_id,
+                'url': self.absolute_url(x['href']),
+                'title': x['title'] or ('Chapter %d' % chap_id),
+            })
         # end for
     # end def
 
-    def download_chapter_body(self, chapter):        #
-        # NOTE: Set `chapter['body_lock'] = True` to disable post-formatting.
-        #       It can be useful in non-english sources, e.g. aixdzs, qidiancom, tiknovel
-        #
-        #       Return an empty body if anything goes wrong. But you should not return `None`.
+    @cleanup_text
+    def download_chapter_body(self, chapter):
         '''Download body of a single chapter and return as clean html format.'''
         logger.info('Downloading %s', chapter['url'])
         soup = self.get_soup(chapter['url'])
 
-        contents = soup.select_one('div.chr-c, #chr-content')
-        for br in contents.select('br'):
-            br.decompose()
-        # end for
+        content = soup.select('#chr-content p')
+        if not content:
+            return ''
+        # end if
 
-        return str(contents)
+        return "".join(map(str, content))
     # end def
 # end class
