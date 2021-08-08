0b749a847c95821e94a469019030e54bccda6160
Sudipto Chandra <dipu.sudipta@gmail.com>
2021-08-08 13:02:56 +0600

    Fix issues after testing

--- lncrawl/sources/lightnovelpub.py
+++ lncrawl/sources/lightnovelpub.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
 import re
-from concurrent import futures
 from urllib.parse import urlparse
 
 from lncrawl.core.crawler import Crawler
@@ -34,31 +33,13 @@ class LightNovelOnline(Crawler):
     # end def
 
     def read_novel_info(self):
-        '''Get novel title, autor, cover etc'''
-        self.novel_url = '%s%s' % (self.base_url, urlparse(self.novel_url).path)
+        self.novel_url = '%s%s' % (self.home_url, urlparse(self.novel_url).path)
 
         logger.debug('Visiting %s', self.novel_url)
         soup = self.get_soup(self.novel_url)
 
-        self.novel_title = soup.select_one(
-            '.novel-info .novel-title').text.strip()
+        self.novel_title = soup.select_one('.novel-info .novel-title').text.strip()
         logger.info('Novel title: %s', self.novel_title)
-
-        try:
-            self.novel_cover = self.absolute_url(
-                soup.select_one('.novel-header .cover img')['data-src'])
-        except:
-            pass
-        # end try
-        logger.info('Novel cover: %s', self.novel_cover)
-
-        try:
-            self.novel_author = soup.select_one(
-                '.author a[href*="/author/"]')['title']
-        except:
-            pass
-        # end try
-        logger.info('Novel author: %s', self.novel_author)
 
         self.novel_id = urlparse(self.novel_url).path.split('/')[-1]
         logger.info('Novel id: %s', self.novel_id)
@@ -68,9 +49,27 @@ class LightNovelOnline(Crawler):
         logger.info('Verification token: %s', self.verificationToken)
 
         try:
-            last_page = soup.select_one('.PagedList-skipToLast a')['href']
-            page_count = int(re.findall(r'page=(\d+)', last_page)[0])
-        except:
+            self.novel_cover = self.absolute_url(
+                soup.select_one('.novel-header .cover img')['data-src'])
+        except Exception as err:
+            logger.debug('Failed to parse novel cover. Error: %s', err)
+        # end try
+        logger.info('Novel cover: %s', self.novel_cover)
+
+        try:
+            self.novel_author = soup.select_one('.author a[href*="/author/"]')['title']
+        except Exception as err:
+            logger.debug('Failed to parse novel author. Error: %s', err)
+        # end try
+        logger.info('Novel author: %s', self.novel_author)
+
+        try:
+            last_page = soup.select_one('.PagedList-skipToLast a')
+            if not last_page:
+                last_page = soup.select('.pagination li a:not([rel="next"])')[-1]
+            page_count = int(re.findall(r'page=(\d+)', last_page['href'])[0])
+        except Exception as err:
+            logger.debug('Failed to parse page count. Error: %s', err)
             page_count = 0
         logger.info('Total pages: %d', page_count)
 
@@ -80,16 +79,17 @@ class LightNovelOnline(Crawler):
             for i in range(page_count + 1)
         ]
 
+        volumes = set([])
         for f in futures:
             for chap in f.result():
                 chap['id'] = len(self.chapters) + 1
                 chap['volume'] = len(self.chapters) // 100 + 1
                 self.chapters.append(chap)
+                volumes.add(chap['volume'])
             # end for
         # end for
 
-        self.volumes = [{'id': i + 1}
-                        for i in range(1 + len(self.chapters) // 100)]
+        self.volumes = [{'id': x} for x in volumes]
     # end def
 
     def extract_chapter_list(self, page):
@@ -115,13 +115,11 @@ class LightNovelOnline(Crawler):
     # end def
 
     def download_chapter_body(self, chapter):
-        '''Download body of a single chapter and return as clean html format.'''
-        logger.info('Downloading %s', chapter['url'])
         soup = self.get_soup(chapter['url'])
         body = soup.select_one('.chapter-content')
         for ads in body.select('div[class*="ad"], script, ins, p[class]'):
             ads.extract()
         # end for
-        return str(body)
+        return self.extract_contents(body)
     # end def
 # end class
