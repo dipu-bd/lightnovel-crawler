2bf36a34be0ed3e2f88862f5e5f0c5dbc1854811
SirGryphin <36343615+SirGryphin@users.noreply.github.com>
2021-09-20 15:46:33 +0100

    fixed zero chapters

--- sources/novelsrock.py
+++ sources/novelsrock.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
-import json
 import logging
-import re
 from urllib.parse import quote_plus
 
 from lncrawl.core.crawler import Crawler
 
 logger = logging.getLogger(__name__)
 search_url = 'https://novelsrock.com/?s=%s&post_type=wp-manga&op=&author=&artist=&release=&adult='
-post_chapter_url = 'https://novelsrock.com/wp-admin/admin-ajax.php'
-
 
 class NovelsRockCrawler(Crawler):
     base_url = 'https://novelsrock.com/'
@@ -58,39 +54,28 @@ class NovelsRockCrawler(Crawler):
             self.novel_author = author[0].text
         logger.info('Novel author: %s', self.novel_author)
 
-        self.novel_id = soup.select_one(
-            '.wp-manga-action-button[data-action=bookmark]')['data-post']
-        logger.info('Novel id: %s', self.novel_id)
-
-        for span in soup.select('.page-content-listing span'):
-            span.extract()
-
-        logger.info('Sending post request to %s', post_chapter_url)
-        response = self.submit_form(post_chapter_url, data={
-            'action': 'manga_get_chapters',
-            'manga': int(self.novel_id)
-        })
-        soup = self.make_soup(response)
-        for a in reversed(soup.select('.wp-manga-chapter > a')):
+        volumes = set()
+        chapters = soup.select('ul.main li.wp-manga-chapter a')
+        for a in reversed(chapters):
             chap_id = len(self.chapters) + 1
-            vol_id = chap_id // 100 + 1
-            if len(self.chapters) % 100 == 0:
-                self.volumes.append({'id': vol_id})
-            # end if
+            vol_id = (chap_id - 1) // 100 + 1
+            volumes.add(vol_id)
             self.chapters.append({
                 'id': chap_id,
                 'volume': vol_id,
-                'title': a.text.strip(),
                 'url':  self.absolute_url(a['href']),
+                'title': a.text.strip() or ('Chapter %d' % chap_id),
             })
         # end for
+
+        self.volumes = [{'id': x} for x in volumes]
     # end def
 
     def download_chapter_body(self, chapter):
         logger.info('Downloading %s', chapter['url'])
         soup = self.get_soup(chapter['url'])
 
-        contents = soup.select('div.reading-content p')
+        contents = soup.select('div.entry-content p')
 
         body = []
         for p in contents:
