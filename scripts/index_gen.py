8228cd6b5d9780c206c344eeed49594cfe5c5623
Sudipto Chandra <dipu.sudipta@gmail.com>
2021-10-07 16:30:30 +0600

    Update index_gen.py

--- scripts/index_gen.py
+++ scripts/index_gen.py
@@ -185,6 +185,7 @@ def process_file(py_file: Path) -> float:
         can_login = 'login' in crawler.__dict__
         can_logout = 'logout' in crawler.__dict__
         can_search = 'search_novel' in crawler.__dict__
+        has_mtl = getattr(crawler, 'machine_translation', False)
         source_id = hashlib.md5(str(crawler).encode('utf8')).hexdigest()
 
         info = {}
@@ -199,6 +200,7 @@ def process_file(py_file: Path) -> float:
         # info['last_commit'] = history[0]
         # info['first_commit'] = history[-1]
         # info['author'] = history[-1]['author']
+        info['has_mtl'] = has_mtl
         info['can_search'] = can_search
         info['can_login'] = can_login
         info['can_logout'] = can_logout
@@ -262,10 +264,13 @@ for url, crawler_id in sorted(INDEX_DATA['supported'].items(), key=lambda x: x[0
     last_update = datetime.fromtimestamp(info['version']).strftime(DATE_FORMAT)
 
     supported += '<tr>'
+
     supported += '<td>'
+    supported += '<span title="Contains machine translations">%s</span>' % ('🤖' if info['has_mtl'] else '')
     supported += '<span title="Supports searching">%s</span>' % ('🔍' if info['can_search'] else '')
     supported += '<span title="Supports login">%s</span>' % ('🔑' if info['can_login'] else '')
     supported += '</td>\n'
+
     supported += '<td><a href="%s" target="_blank">%s</a></td>\n' % (url, url)
     supported += '<td><a href="%s" title="%s">%d</a></td>\n' % (source_url, last_update, info['total_commits'])
     supported += '<td>%s</td>\n' % ' '.join([
