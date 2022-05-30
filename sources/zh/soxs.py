9e7f5459f60cea10ea68d96768fb1d42bb28666d
Sudipto Chandra <dipu.sudipta@gmail.com>
2022-05-30 14:35:34 +0400

    Update soxs.py

--- sources/zh/soxs.py
+++ sources/zh/soxs.py
@@ -4,41 +4,16 @@ from lncrawl.core.crawler import Crawler
 
 
 logger = logging.getLogger(__name__)
-# search_url = 'https://tw.m.ixdzs.com/search?k=%s'
 
 
 class Soxc(Crawler):
-
     base_url = ['https://www.soxs.cc/']
 
-
-#     def search_novel(self, query):
-#         query = query.lower().replace(' ', '+')
-#         soup = self.get_soup(search_url % query)
-#         results = []
-
-#         for data in soup.select('.xiaoshuo h1'):
-#             title = data.select_one('h3 a').get_text().strip()
-#             url = self.absolute_url(
-#                 data.select_one('h3 a')['href']
-#             )
-#             results.append(
-#                 {
-#                     'title': title,
-#                     'url': url,
-#                 }
-#             )
-#         return results
-    # end def
-
     def read_novel_info(self):
-        '''Get novel title, author, cover etc'''
-        logger.debug('Visiting %s', self.novel_url)
         self.novel_url = self.novel_url.replace('/book/', '/')
         self.novel_url = self.novel_url.replace('.html', '/')
         soup = self.get_soup(self.novel_url)
         
-
         possible_title = soup.select_one('.xiaoshuo h1')
         assert possible_title, 'No novel title'
         self.novel_title =  possible_title.get_text()
@@ -55,29 +30,23 @@ class Soxc(Crawler):
         logger.info('Getting chapters...')
         for chapter in soup.select(".novel_list dd a"):       
             url = self.absolute_url(chapter['href'])
-    
-            
             chap_id = len(self.chapters) + 1
             if len(self.chapters) % 100 == 0:
-                vol_id = chap_id//100 + 1
-                vol_title = 'Volume ' + str(vol_id)
+                vol_id = len(self.chapters) // 100 + 1
                 self.volumes.append({
-                    'id': vol_id,
-                    'title': vol_title
+                    'id': vol_id
                 })
+            # end if
            
             self.chapters.append({
                 'id': chap_id,
-                #'title': title,
                 'url': url,
-                'volume': vol_id
+                'volume': vol_id,
             })
+        # end for
     # end def
 
     def download_chapter_body(self, chapter):
-
-        logger.info(f"Downloading {chapter['url']}")
-
         soup = self.get_soup(chapter['url'])
         title = soup.select_one('.read_title h1').text.strip()
         chapter['title'] = title
