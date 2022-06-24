2858f9355aad2d4e153255c1b43f1ddfb6ae389f
SirGryphin <36343615+SirGryphin@users.noreply.github.com>
2022-06-24 21:21:37 +0100

    added source

---
+++ sources/en/n/novelzec.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from lncrawl.core.crawler import Crawler
+
+logger = logging.getLogger(__name__)
+
+
+class NovelZec(Crawler):
+    base_url = "https://novelzec.com/"
+
+    def read_novel_info(self):
+        logger.debug("Visiting %s", self.novel_url)
+        soup = self.get_soup(self.novel_url)
+
+        possible_title = soup.select_one(".entry-header h1")
+        assert possible_title, "No novel title"
+        self.novel_title = possible_title.text.strip()
+        logger.info("Novel title: %s", self.novel_title)
+
+        authors = soup.select('.entry-header span a[href*="/author/"]')
+        self.novel_author = ", ".join([a.text.strip() for a in authors])
+        logger.info("Novel author: %s", self.novel_author)
+
+        possible_image = soup.select_one(".entry-header img")
+        if possible_image:
+            self.novel_cover = self.absolute_url(possible_image["src"])
+        logger.info("Novel cover: %s", self.novel_cover)
+
+        for a in reversed(soup.select("#chap-list li a")):
+            chap_id = len(self.chapters) + 1
+            vol_id = len(self.chapters) // 100 + 1
+            if len(self.chapters) % 100 == 0:
+                self.volumes.append({"id": vol_id})
+            # end if
+            self.chapters.append(
+                {
+                    "id": chap_id,
+                    "volume": vol_id,
+                    "title": a.text.strip(),
+                    "url": self.absolute_url(a["href"]),
+                }
+            )
+        # end for
+
+    # end def
+
+    def download_chapter_body(self, chapter):
+        soup = self.get_soup(chapter["url"])
+        contents = soup.select_one(".content-story")
+        return self.cleaner.extract_contents(contents)
+
+    # end def
+
+
+# end class
