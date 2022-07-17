23dc9ae511a545776ea0e64f944275774643af8d
Sudipto Chandra <dipu.sudipta@gmail.com>
2022-07-17 15:51:42 +0400

    Fix comrademao #1353

--- sources/en/c/fu_kemao.py
+++ sources/en/c/fu_kemao.py
@@ -18,8 +18,12 @@ class Fu_kCom_ademao(Crawler):
     base_url = b64decode("aHR0cHM6Ly9jb21yYWRlbWFvLmNvbS8=".encode()).decode()
 
     def initialize(self) -> None:
+        self.cleaner.bad_tags.update([
+            'header'
+        ])
         self.cleaner.bad_css.update([
             '#ad',
+            '#bookmark'
             '#div-gpt-ad-comrademaocom35917',
             '#div-gpt-ad-comrademaocom35918',
         ])
@@ -49,7 +53,7 @@ class Fu_kCom_ademao(Crawler):
         self.novel_title = possible_title.text.rsplit(r'\u2013', 1)[0].strip()
         logger.debug('Novel title = %s', self.novel_title)
 
-        possible_image = soup.select_one('img.attachment-post-thumbnail')
+        possible_image = soup.select_one('img.attachment-post-thumbnail, .seriedetailcontent img')
         if isinstance(possible_image, Tag):
             self.novel_cover = self.absolute_url(possible_image['src'])
         logger.info('Novel cover: %s', self.novel_cover)
@@ -58,23 +62,11 @@ class Fu_kCom_ademao(Crawler):
         # logger.info('Novel author: %s', self.novel_author)
 
         logger.info('Getting chapters...')
-        pagination = soup.select('.pagination-list .pagination-link')[-1]
-        page_count = int(pagination.text) if isinstance(pagination, Tag) else 1
-        logger.info('# page count: %d', page_count)
- 
-        futures_to_check = []
-        novel_url = self.novel_url.split('?')[0].strip('/')
-        for i in range(page_count - 1):
-            future = self.executor.submit(
-                self.get_soup, novel_url + '/page/%d' % (page_count - i))
-            futures_to_check.append(future)
-        # end for
-        futures_to_check.append(self.executor.submit(lambda: soup))
-
-        volumes = set()
-        for future in futures_to_check:
-            soup = future.result()
-            for a in reversed(soup.select('.container table a')):
+        chapter_links = soup.select('#chapterlist li a')
+        paginations = soup.select('.pagination-list .pagination-link')
+        if len(chapter_links):
+            volumes = set()
+            for a in reversed(chapter_links):
                 chap_id = len(self.chapters) + 1
                 vol_id = len(self.chapters) // 100 + 1
                 volumes.add(vol_id)
@@ -85,14 +77,43 @@ class Fu_kCom_ademao(Crawler):
                     'title': a.text.strip(),
                 })
             # end for
-        # end for
+        elif len(paginations):
+            pagination = paginations[-1]
+            page_count = int(pagination.text) if isinstance(pagination, Tag) else 1
+            logger.info('# page count: %d', page_count)
+    
+            futures_to_check = []
+            novel_url = self.novel_url.split('?')[0].strip('/')
+            for i in range(page_count - 1):
+                future = self.executor.submit(
+                    self.get_soup, novel_url + '/page/%d' % (page_count - i))
+                futures_to_check.append(future)
+            # end for
+            futures_to_check.append(self.executor.submit(lambda: soup))
+
+            volumes = set()
+            for future in futures_to_check:
+                soup = future.result()
+                for a in reversed(soup.select('.container table a')):
+                    chap_id = len(self.chapters) + 1
+                    vol_id = len(self.chapters) // 100 + 1
+                    volumes.add(vol_id)
+                    self.chapters.append({
+                        'id': chap_id,
+                        'volume': vol_id,
+                        'url': self.absolute_url(str(a['href'])),
+                        'title': a.text.strip(),
+                    })
+                # end for
+            # end for
+        # end if 
 
         self.volumes = [{'id': x} for x in volumes]
     # end def
     
     def download_chapter_body(self, chapter):
         soup = self.get_soup(chapter['url'])
-        body = soup.select_one("#content")
+        body = soup.select_one("#content, #chaptercontent")
         return self.cleaner.extract_contents(body)
     # end def
 # end class
