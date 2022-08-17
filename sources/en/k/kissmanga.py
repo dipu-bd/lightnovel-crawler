598f40c4a8bd7e54a1783a91d3b6a66c19b76ed4
Andrey Tokarev <andrey777tokarev@yandex.ru>
2022-08-17 13:11:21 +0300

    Update kissmanga.py

--- sources/en/k/kissmanga.py
+++ sources/en/k/kissmanga.py
@@ -6,6 +6,7 @@ from lncrawl.core.crawler import Crawler
 logger = logging.getLogger(__name__)
 
 class KissMangaCrawler(Crawler):
+    has_manga = True
     base_url = [
         'https://kissmanga.in/'
     ]
