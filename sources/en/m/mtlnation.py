1c9f814f04920a5042932dfd811095663b892aff
Andrey Tokarev <andrey777tokarev@yandex.ru>
2022-08-18 21:22:37 +0300

    Fix https://mtlnation.com/
    Fix https://github.com/dipu-bd/lightnovel-crawler/issues/1453

--- sources/en/m/mtlnation.py
+++ sources/en/m/mtlnation.py
@@ -51,7 +51,11 @@ class MTLNation(Crawler):
     def read_novel_info(self):
         slug = urlparse(self.novel_url).path.split('/')[-1]
         data = self.get_json(f'https://api.mtlnation.com/api/v2/novels/{slug}')
-
+        
+        if (len(data['data']) > 1):
+            data['data'] = data['data'][0]
+        # end if
+        
         self.novel_title = data['data']['title']
         self.novel_author = data['data']['author']
         self.novel_cover = 'https://api.mtlnation.com/media/' + data['data']['cover']
