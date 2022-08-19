c410ceb2c1980b189c3b8ad5f13c214c5b8ab801
Sudipto Chandra <dipu.sudipta@gmail.com>
2022-08-19 08:08:41 +0400

    Add has_manga = True

--- sources/en/i/isekaiscaneu.py
+++ sources/en/i/isekaiscaneu.py
@@ -6,6 +6,7 @@ from lncrawl.core.crawler import Crawler
 logger = logging.getLogger(__name__)
 
 class IsekaiScanEUCrawler(Crawler):
+    has_manga = True
     base_url = [
         'https://isekaiscan.eu/'
     ]
