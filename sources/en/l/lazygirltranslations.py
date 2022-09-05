cbdb01e5ec2749c7aa62d6cf78853d54b16ec629
Andrey Tokarev <andrey777tokarev@yandex.ru>
2022-09-05 12:29:47 +0300

    Create lazygirltranslations.py

---
+++ sources/en/l/lazygirltranslations.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import logging
+from lncrawl.core.crawler import Crawler
+
+logger = logging.getLogger(__name__)
+
+class LazyGirlTranslationsCrawler(Crawler):
+    base_url = 'https://lazygirltranslations.com/'
+
+    def read_novel_info(self):
+        soup = self.get_soup(self.novel_url)
+
+        self.novel_title = soup.select_one('h1.entry-title').text.strip()
+        
+        cover_img = soup.select_one('.entry-content .wp-block-image img')      
+        if cover_img:
+            self.novel_cover = self.absolute_url(cover_img['data-ezsrc'])
+        # end if
+
+        first_p = soup.select_one("div.entry-content p")
+
+        for text in first_p.get_text(separator='\n', strip=True).split('\n'):
+            if text.startswith('Author'):
+                self.novel_author = text.replace("Author: ", "")
+                break
+
+        for a in soup.select('.wp-block-column a'):
+            chap_id = len(self.chapters) + 1
+            vol_id = 1 + len(self.chapters) // 100
+            if chap_id % 100 == 1:
+                self.volumes.append({"id": vol_id})
+            # end if
+            self.chapters.append({
+                'id': chap_id,
+                'volume': vol_id,
+                'url':  self.absolute_url(a['href']),
+                'title': a.text.strip(),
+            })
+        # end for
+    # end def
+
+    def download_chapter_body(self, chapter):
+        soup = self.get_soup(chapter['url'])
+        contents = soup.select('.entry-content p')[2:-1]
+        return ''.join([str(p) for p in contents])
+    # end def
+# end class
