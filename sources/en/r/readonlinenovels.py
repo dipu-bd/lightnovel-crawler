1027a47f89cfd4d81c3a796e58b0d8a00f7542e0
Andrey Tokarev <andrey777tokarev@yandex.ru>
2022-09-07 12:18:10 +0300

    Fix https://readonlinenovels.com/
    https://github.com/dipu-bd/lightnovel-crawler/issues/1553

--- sources/en/r/readonlinenovels.py
+++ sources/en/r/readonlinenovels.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 import logging
-import re
 
 from lncrawl.core.crawler import Crawler
 
 logger = logging.getLogger(__name__)
-search_url = 'http://readonlinenovels.com/novel/search/?keywords=%s'
+search_url = 'https://readonlinenovels.com/novel/search/?keywords=%s'
 
 
 class ReadOnlineNovelsCrawler(Crawler):
-    base_url = ['http://readonlinenovels.com/']
-
-    def initialize(self):
-        self.home_url = 'http://readonlinenovels.com/'
-    # end def
+    base_url = ['http://readonlinenovels.com/',
+                'https://readonlinenovels.com/', ]
 
     # NOTE: Disabled because it takes too long
     # def search_novel(self, query):
@@ -52,28 +48,21 @@ class ReadOnlineNovelsCrawler(Crawler):
             self.novel_cover = self.absolute_url(possible_image['src'])
         logger.info('Novel cover: %s', self.novel_cover)
 
-        # Extract volume-wise chapter entries
         for a in soup.select('div.slide-item a'):
             chap_id = len(self.chapters) + 1
             vol_id = 1 + len(self.chapters) // 100
             if len(self.volumes) < vol_id:
-                self.volumes.append({ 'id': vol_id })
-            # end if
+                self.volumes.append({'id': vol_id})
+
             self.chapters.append({
                 'id': chap_id,
                 'volume': vol_id,
                 'title': a.text.strip(),
-                'url':  self.absolute_url(a['href']),
+                'url': self.absolute_url(a['href']),
             })
-        # end for
-
-    # end def
 
     def download_chapter_body(self, chapter):
         soup = self.get_soup(chapter['url'])
         contents = soup.select('div.read-context p')
         body = [str(p) for p in contents if p.text.strip()]
         return '<p>' + '</p><p>'.join(body) + '</p>'
-    # end def
-
-# end class
