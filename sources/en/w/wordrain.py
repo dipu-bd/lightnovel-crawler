265991419dc32245ebb7489510e358f23b217507
dipu-bd <dipu.sudipta@gmail.com>
2022-09-27 23:56:39 +0400

    Fix two sources

--- sources/en/w/wordrain.py
+++ sources/en/w/wordrain.py
@@ -6,18 +6,19 @@ from concurrent.futures import ThreadPoolExecutor
 from lncrawl.core.crawler import Crawler
 
 logger = logging.getLogger(__name__)
-search_url = 'https://wordrain69.com/?s=%s'
-post_chapter_url = 'https://wordrain69.com/wp-admin/admin-ajax.php'
+search_url = "https://wordrain69.com/?s=%s"
+post_chapter_url = "https://wordrain69.com/wp-admin/admin-ajax.php"
 
 
 class WordRain(Crawler):
-    base_url = 'https://wordrain69.com'
+    base_url = "https://wordrain69.com"
 
     def initialize(self):
         self.executor = ThreadPoolExecutor(max_workers=7)
 
     # NOTE: Site search doesn't work. So this won't work.
-    """ def search_novel(self, query):
+    """
+    def search_novel(self, query):
         query = query.lower().replace(' ', '+')
         soup = self.get_soup(search_url % query)
 
@@ -33,61 +34,62 @@ class WordRain(Crawler):
             })
 
         return results
+    """
 
     def read_novel_info(self):
-        logger.debug('Visiting %s', self.novel_url)
+        logger.debug("Visiting %s", self.novel_url)
         soup = self.get_soup(self.novel_url)
 
         possible_title = soup.select_one('meta[property="og:title"]')
-        assert isinstance(possible_title, Tag), 'No novel title'
-        self.novel_title = possible_title['content']
-        logger.info('Novel title: %s', self.novel_title)
+        assert isinstance(possible_title, Tag), "No novel title"
+        self.novel_title = possible_title["content"]
+        logger.info("Novel title: %s", self.novel_title)
 
         possible_image = soup.select_one('meta[property="og:image"]')
         if isinstance(possible_image, Tag):
-            self.novel_cover = possible_image['content']
-        logger.info('Novel cover: %s', self.novel_cover)
+            self.novel_cover = possible_image["content"]
+        logger.info("Novel cover: %s", self.novel_cover)
 
-        self.novel_author = ' '.join(
+        self.novel_author = " ".join(
             [
                 a.text.strip()
                 for a in soup.select('.author-content a[href*="manga-translator"]')
             ]
         )
-        logger.info('%s', self.novel_author)
+        logger.info("%s", self.novel_author)
 
         self.novel_id = soup.select_one(
-            '.wp-manga-action-button[data-action=bookmark]'
-        )['data-post']
-        logger.info('Novel id: %s', self.novel_id)
+            ".wp-manga-action-button[data-action=bookmark]"
+        )["data-post"]
+        logger.info("Novel id: %s", self.novel_id)
 
-        logger.info('Sending post request to %s', post_chapter_url)
+        logger.info("Sending post request to %s", post_chapter_url)
         response = self.submit_form(
             post_chapter_url,
-            data={'action': 'manga_get_chapters', 'manga': int(self.novel_id)},
+            data={"action": "manga_get_chapters", "manga": int(self.novel_id)},
         )
         soup = self.make_soup(response)
-        for a in reversed(soup.select('.wp-manga-chapter > a')):
+        for a in reversed(soup.select(".wp-manga-chapter > a")):
             chap_id = len(self.chapters) + 1
             vol_id = chap_id // 100 + 1
             if len(self.chapters) % 100 == 0:
-                self.volumes.append({'id': vol_id})
+                self.volumes.append({"id": vol_id})
             self.chapters.append(
                 {
-                    'id': chap_id,
-                    'volume': vol_id,
-                    'title': a.text.strip(),
-                    'url': self.absolute_url(a['href']),
+                    "id": chap_id,
+                    "volume": vol_id,
+                    "title": a.text.strip(),
+                    "url": self.absolute_url(a["href"]),
                 }
             )
 
     def download_chapter_body(self, chapter):
-        soup = self.get_soup(chapter['url'])
+        soup = self.get_soup(chapter["url"])
 
-        contents = soup.select_one('div.text-left')
+        contents = soup.select_one("div.text-left")
 
         for bad in contents.select(
-            'h3, .code-block, script, .adsbygoogle, .adsense-code, .sharedaddy, a'
+            "h3, .code-block, script, .adsbygoogle, .adsense-code, .sharedaddy, a"
         ):
             bad.extract()
 
