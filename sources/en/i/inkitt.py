81c62e111074bf7572cebf9776c83238b05d3422
Andrey Tokarev <andrey777tokarev@yandex.ru>
2022-09-28 17:22:36 +0300

    Search

--- sources/en/i/inkitt.py
+++ sources/en/i/inkitt.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*-
 import json
 import logging
+from typing import List
+from urllib.parse import quote
 
 from bs4 import Tag
 
 from lncrawl.core.crawler import Crawler
 from lncrawl.core.exeptions import LNException
+from lncrawl.models import SearchResult
 
 logger = logging.getLogger(__name__)
 
 
 class InkittCrawler(Crawler):
     base_url = ["https://www.inkitt.com/"]
+
+    def search_novel(self, query) -> List[SearchResult]:
+        search_json = self.get_json(
+            f"{self.home_url}api/2/search/title?q={quote(query)}&page=1"
+        )
+
+        return [self.parse_search_item(story) for story in search_json["stories"]]
+
+    def parse_search_item(self, story) -> SearchResult:
+        return SearchResult(
+            title=story["title"],
+            url=self.absolute_url(f"/stories/{story['id']}"),
+            info=f"Chapters: {story['chapters_count']}, Status: {story['story_status']}",
+        )
 
     def read_novel_info(self):
         soup = self.get_soup(self.novel_url)
