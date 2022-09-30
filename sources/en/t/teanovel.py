8b1f7e75fbcc24937018fbb4c8da8cd9ce4b4b76
Andrey Tokarev <andrey777tokarev@yandex.ru>
2022-09-30 15:16:30 +0300

    Refactoring

--- sources/en/t/teanovel.py
+++ sources/en/t/teanovel.py
@@ -2,7 +2,10 @@
 import json
 import logging
 
+from bs4 import Tag
+
 from lncrawl.core.crawler import Crawler
+from lncrawl.core.exeptions import LNException
 
 logger = logging.getLogger(__name__)
 
@@ -13,20 +16,23 @@ class TeaNovelCrawler(Crawler):
     def read_novel_info(self):
         soup = self.get_soup(self.novel_url)
 
-        script_data = soup.select_one('script[id="__NEXT_DATA__"]').get_text()
-        json_data = json.loads(script_data)
+        script_tag = soup.select_one("script#__NEXT_DATA__")
+        if not isinstance(script_tag, Tag):
+            raise LNException("No script data found")
 
-        buildId = json_data["buildId"]
-        book_data = json_data["props"]["pageProps"]["novelData"]["novel"]
+        next_data = json.loads(script_tag.get_text())
 
-        self.novel_title = book_data["name"]
-        self.novel_author = book_data["author"]
+        build_id = next_data["buildId"]
+        novel_data = next_data["props"]["pageProps"]["novelData"]["novel"]
+
+        self.novel_title = novel_data["name"]
+        self.novel_author = novel_data["author"]
 
         # img_tag = soup.select_one("main img[src*='_next/']")
         # if isinstance(img_tag, Tag):
         #     self.novel_cover = self.absolute_url(img_tag["src"])
 
-        slug = book_data["slug"]
+        slug = novel_data["slug"]
 
         toc_url = f"{self.home_url}api/chapters/{slug}?slug={slug}&orderBy=asc"
         toc_json = self.get_json(toc_url)
@@ -39,7 +45,7 @@ class TeaNovelCrawler(Crawler):
                         "id": chapter_id,
                         "title": f"Chapter {chapter_id}: {chapter['title']}",
                         "url": (
-                            f"{self.home_url}_next/data/{buildId}/novel/{slug}/{chapter['slug']}.json"
+                            f"{self.home_url}_next/data/{build_id}/novel/{slug}/{chapter['slug']}.json"
                         ),
                     }
                 )
