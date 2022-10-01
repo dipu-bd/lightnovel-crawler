6c6544c81d9e74aeec8d5b4c70a0fe70374d30d5
zerty <zerty@users.noreply.github.com>
2022-10-01 09:27:33 +0200

    Forgotten XML tag

--- lncrawl/binders/epub.py
+++ lncrawl/binders/epub.py
@@ -12,7 +12,7 @@ except ImportError:
 
 logger = logging.getLogger(__name__)
 
-NEW_COVER_XML = b"""
+NEW_COVER_XML = b"""<?xml version="1.0" encoding="UTF-8"?>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops" lang="en" xml:lang="en">
  <head>
  </head>
