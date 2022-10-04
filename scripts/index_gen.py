1ef90b4fb5f66508c696afbfa6412a1184d436a8
Sudipto Chandra <dipu.sudipta@gmail.com>
2022-10-04 22:01:14 +0400

    Use diff-filter ACT for test #1686

--- scripts/index_gen.py
+++ scripts/index_gen.py
@@ -161,7 +161,7 @@ def search_user_by(query):
 def git_history(file_path):
     try:
         cmd = (
-            'git log --follow --diff-filter=ACMT --pretty="%%at||%%aN||%%aE||%%s" "%s"'
+            'git log --follow --diff-filter=ACT --pretty="%%at||%%aN||%%aE||%%s" "%s"'
             % file_path
         )
         # cmd = 'git log -1 --diff-filter=ACMT --pretty="%%at||%%aN||%%aE||%%s" "%s"' % file_path
