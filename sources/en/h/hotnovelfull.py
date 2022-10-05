18eee98f3f152a8d822101ca3c037c14f97f9b8a
SirGryphin <36343615+SirGryphin@users.noreply.github.com>
2022-10-05 01:13:13 +0100

    clean bad text

--- sources/en/h/hotnovelfull.py
+++ sources/en/h/hotnovelfull.py
@@ -8,3 +8,8 @@ logger = logging.getLogger(__name__)
 
 class HotNovelFullCrawler(NovelFullTemplate):
     base_url = ["https://hotnovelfull.com/"]
+
+    def initialize(self) -> None:
+            self.cleaner.bad_text_regex.update(
+                ["This chapter upload first at NovelNext.com"]
+            )
