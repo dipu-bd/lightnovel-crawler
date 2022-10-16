fc48f01c230b09c06c1ce80cd6ec2e627aec1c53
SirGryphin <36343615+SirGryphin@users.noreply.github.com>
2022-10-17 00:00:27 +0100

    add more bad text

--- sources/en/n/novelupdatescom.py
+++ sources/en/n/novelupdatescom.py
@@ -24,6 +24,7 @@ class NovelUpdatesCom(Crawler):
             {
                 "a": [
                     r"ToC",
+                    r"TOC",
                     r"<<",
                     r">>"
                 ]
