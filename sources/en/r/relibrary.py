a2064ee6aba59d4268606f4b7a5c41ecf67dbf3d
Sudipto Chandra <dipu.sudipta@gmail.com>
2022-11-24 08:27:58 +0400

    Update relibrary

--- sources/en/r/relibrary.py
+++ sources/en/r/relibrary.py
@@ -10,7 +10,9 @@ logger = logging.getLogger(__name__)
 
 
 class relibCrawler(ChapterOnlySoupTemplate):
-    base_url = ["https://re-library.com/"]
+    base_url = [
+        "https://re-library.com/",
+    ]
 
     def initialize(self) -> None:
         self.init_executor(1)
@@ -22,27 +24,28 @@ class relibCrawler(ChapterOnlySoupTemplate):
                 "a[href*='re-library.com']",
             ]
         )
-        self.cleaner.bad_tag_text_pairs.update({"h2": "References"})
+        self.cleaner.bad_tag_text_pairs.update(
+            {
+                "h2": "References",
+            }
+        )
 
     def parse_title(self, soup: BeautifulSoup) -> str:
         tag = soup.select_one(".entry-title")
-        assert tag
         return tag.text.strip()
 
     def parse_cover(self, soup: BeautifulSoup) -> str:
         tag = soup.select_one(".entry-content table img")
-        assert tag
-        if tag.has_attr("data-src"):
-            return self.absolute_url(tag["data-src"])
-        if tag.has_attr("src"):
-            return self.absolute_url(tag["src"])
+        src = tag.get("data-src") or tag.get("src")
+        return self.absolute_url(src)
 
     def parse_authors(self, soup: BeautifulSoup) -> Generator[str, None, None]:
         for a in soup.select_one(".entry-content").select("a[href*='/nauthor/']"):
             yield a.text.strip()
 
     def select_chapter_tags(self, soup: BeautifulSoup) -> Generator[Tag, None, None]:
-        yield from soup.select(".page_item > a ")
+        print("hello")
+        yield from soup.select(".page_item > a")
 
     def parse_chapter_item(self, tag: Tag, id: int) -> Chapter:
         return Chapter(
@@ -52,6 +55,4 @@ class relibCrawler(ChapterOnlySoupTemplate):
         )
 
     def select_chapter_body(self, soup: BeautifulSoup) -> Tag:
-        tag = soup.select_one(".entry-content")
-        assert tag
-        return tag
+        return soup.select_one(".entry-content")
