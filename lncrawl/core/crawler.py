8c1253896774ecf658c7bc5082151c42af6f022b
dipu-bd <dipu.sudipta@gmail.com>
2023-02-02 20:12:48 +0400

    Fix self.file_path issue

--- lncrawl/core/crawler.py
+++ lncrawl/core/crawler.py
@@ -2,7 +2,6 @@ import hashlib
 import logging
 from abc import abstractmethod
 from typing import Generator, List, Optional
-from pathlib import Path
 
 from ..models import Chapter, SearchResult, Volume
 from .cleaner import TextCleaner
@@ -17,6 +16,7 @@ class Crawler(Scraper):
     has_manga = False
     has_mtl = False
     base_url: List[str]
+    novel_language = ""
 
     # ------------------------------------------------------------------------- #
     # Constructor & Destructors
@@ -46,15 +46,6 @@ class Crawler(Scraper):
         self.is_rtl = False
         self.novel_synopsis = ""
         self.novel_tags = []
-
-        # The folder in which the crawler is located
-        # If the source contains multiple languages it need to be overridden by crawlers to avoid having language='multi'
-        crawler_path = Path(self.file_path)
-        # English source are store inside a second folder
-        if crawler_path.parent.parent.name == 'en':
-            self.novel_language = 'en'
-        else :
-            self.novel_language = crawler_path.parent.name
 
         # Each item must contain these keys:
         # `id` - 1 based index of the volume
