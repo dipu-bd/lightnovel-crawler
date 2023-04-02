988c81d25997581d28b4c088efd41d642c272fc9
Jirawat Boonsumret <jirawat859@hotmail.com>
2023-04-02 13:38:55 +0700

    fix selenium unable to connect to chrome in headless mode (for v109+)

--- lncrawl/webdriver/local.py
+++ lncrawl/webdriver/local.py
@@ -83,7 +83,7 @@ def create_local(
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--start-maximized")
         options.add_argument("--no-sandbox")
-        options.add_argument("--headless")
+        options.add_argument("--headless=new")
     else:
         width = max(640, Screen.view_width * 3 // 4)
         height = max(480, Screen.view_height * 3 // 4)
