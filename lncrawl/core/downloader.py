714c7f4b40897778825d669daf4a37f27187fbfa
jere344 <86294972+jere344@users.noreply.github.com>
2023-04-05 18:29:36 -0400

    Improve image download
    Do not convert images if not needed and optimize storage.
    
    TODO : perhaps a new argument for image quality 
    img.save(image_file, "JPEG", optimized=True, quality=75)
    quality is from 0-100 default 75

--- lncrawl/core/downloader.py
+++ lncrawl/core/downloader.py
@@ -109,7 +109,9 @@ def _fetch_content_image(app, url, image_file):
         try:
             img = app.crawler.download_image(url)
             os.makedirs(os.path.dirname(image_file), exist_ok=True)
-            img.convert("RGB").save(image_file, "JPEG")
+            if img.mode in ("RGBA", "P"): 
+                img = img.convert("RGB")
+            img.save(image_file, "JPEG", optimized=True)
             logger.debug("Saved image: %s", image_file)
         finally:
             app.progress += 1
