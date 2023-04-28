d8aa37b7431e5c32c9e6e5711b91fa0091f45446
Yoga Setiawan <yogainformatika@gmail.com>
2023-04-28 16:14:46 +0700

    Fix indowebnovel info and list chapters

--- sources/id/indowebnovel.py
+++ sources/id/indowebnovel.py
@@ -18,21 +18,21 @@ class IndowebnovelCrawler(Crawler):
         logger.debug("Visiting %s", self.novel_url)
         soup = self.get_soup(self.novel_url)
 
-        possible_title = soup.select_one("h1.entry-title")
+        possible_title = soup.select_one("div.series-title")
         assert possible_title, "No novel title"
-        self.novel_title = possible_title.text
+        self.novel_title = possible_title.get_text(" ")
         logger.info("Novel title: %s", self.novel_title)
 
         self.novel_author = "Translated by Indowebnovel"
         logger.info("Novel author: %s", self.novel_author)
 
-        possible_image = soup.select_one("div.lightnovel-thumb img")
+        possible_image = soup.select_one("div.series-thumb img")
         if possible_image:
             self.novel_cover = self.absolute_url(possible_image["src"])
         logger.info("Novel cover: %s", self.novel_cover)
 
         # Extract volume-wise chapter entries
-        chapters = soup.select("div.lightnovel-episode ul li a")
+        chapters = soup.select("div.series-chapter ul li a")
 
         chapters.reverse()
 
@@ -46,12 +46,12 @@ class IndowebnovelCrawler(Crawler):
                     "id": chap_id,
                     "volume": vol_id,
                     "url": self.absolute_url(a["href"]),
-                    "title": a.text.strip() or ("Chapter %d" % chap_id),
+                    "title": a.get_text(" - ", strip=True) or ("Chapter %d" % chap_id),
                 }
             )
 
     def download_chapter_body(self, chapter):
         soup = self.get_soup(chapter["url"])
-        contents = soup.select("#main article p")
+        contents = soup.select("div.content div.reader p")
         body = [str(p) for p in contents if p.text.strip()]
         return "<p>" + "</p><p>".join(body) + "</p>"
