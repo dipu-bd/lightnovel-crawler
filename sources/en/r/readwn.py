d97cdef022ebbba6ed5c7d73a38873e7c2698dfa
jere344 <86294972+jere344@users.noreply.github.com>
2023-07-06 12:47:03 +0200

    Update readwn.py
    https://readwn.com/ now redirect to https://www.wuxiap.com/

--- sources/en/r/readwn.py
+++ sources/en/r/readwn.py
@@ -6,4 +6,7 @@ logger = logging.getLogger(__name__)
 
 class ReadWNCrawler(NovelMTLTemplate):
     has_mtl = True
-    base_url = "https://www.readwn.com/"
+    base_url = [
+        "https://www.readwn.com/"
+        "https://www.wuxiap.com/"
+    ]
