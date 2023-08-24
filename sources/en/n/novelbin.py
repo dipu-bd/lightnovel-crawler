2b2253819a4948ee30e390b5c9f67b000979ee63
zerty <zerty@users.noreply.github.com>
2023-08-24 11:06:39 +0200

    Rate limiting example in a crawler (NovelBin)

--- sources/en/n/novelbin.py
+++ sources/en/n/novelbin.py
@@ -8,3 +8,6 @@ logger = logging.getLogger(__name__)
 
 class NovelbinCrawler(NovelFullTemplate):
     base_url = ["https://novelbin.com/"]
+
+    def initialize(self) -> None:
+        self.init_executor(ratelimit=0.99)
