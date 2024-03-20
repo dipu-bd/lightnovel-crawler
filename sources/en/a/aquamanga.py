9b533ce4ba41c445f5c2dd88ad7c7a5392ede294
zGadli <kartikgadli303@gmail.com>
2024-03-20 17:50:40 +0530

    Revert "Update aquamanga.py"
    This reverts commit 5f021d96ed562ed1ff0d7d00ae34f7c45a2f7922.

--- sources/en/a/aquamanga.py
+++ sources/en/a/aquamanga.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 class AquaMangaCrawler(Crawler):
     has_manga = True
-    base_url = ["https://aquamanga.com/", "https://aquamanga.org/"]
+    base_url = ["https://aquamanga.com/"]
 
     search_url = "%s?s=%s&post_type=wp-manga&author=&artist=&release="
 
