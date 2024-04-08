71621ef0d6a8a5b04ee95842612ed8fcc36f340e
zGadli <kartikgadli303@gmail.com>
2024-04-08 14:44:00 +0530

    Update novel_search.py

--- lncrawl/core/novel_search.py
+++ lncrawl/core/novel_search.py
@@ -45,13 +45,6 @@ def _combine_results(results: List[SearchResult]) -> List[CombinedSearchResult]:
     combined: Dict[str, List[SearchResult]] = {}
     for item in results:
 
-        if item.title is None:
-            logger.warn(f'Title is type None in {item}')
-            continue
-        elif isinstance(item.title, Tag):
-            logger.warn(f'Title is type Tag in {item}')
-            item.title = item.title.get_text()
-
         key = slugify(item.title)
 
         if len(key) <= 2:
