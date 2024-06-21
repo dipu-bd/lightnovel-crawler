738ea4fe16a45dc726a21f92d66b5ab9db7268c3
zGadli <kartikgadli303@gmail.com>
2024-06-21 12:22:20 +0530

    change rate limit

--- sources/zh/69shuba.py
+++ sources/zh/69shuba.py
@@ -43,7 +43,7 @@ class sixnineshu(Crawler):
     def initialize(self):
         # the default lxml parser cannot handle the huge gbk encoded sites (fails after 4.3k chapters)
         self.init_parser("html.parser")
-        self.init_executor(ratelimit=3)
+        self.init_executor(ratelimit=20)
 
     def search_novel(self, query):
         query = urllib.parse.quote(query.encode("gbk"))
