ca918a2f53907b3b06ff05a6aeb6984c6b992899
zGadli <96724117+zGadli@users.noreply.github.com>
2024-07-08 03:10:39 +0530

    Fix navigation. (#2409)
    When left or right arrow key is held down the you will quickly navigate to next or previous chapters.

--- lncrawl/assets/web/script.js
+++ lncrawl/assets/web/script.js
@@ -8,9 +8,46 @@ function goToHref(el) {
   window.location.href = href;
 }
 
-window.addEventListener("keyup", function (evt) {
-  clearInterval(keyPressTimer);
+function startNavigation(direction) {
+  if (direction === "left") {
+    keyPressTimer = setTimeout(() => {
+      navigationInterval = setInterval(() => {
+        goToHref(document.querySelector("a.prev-button"));
+      }, 50);
+    }, 100);
+  } else if (direction === "right") {
+    keyPressTimer = setTimeout(() => {
+      navigationInterval = setInterval(() => {
+        goToHref(document.querySelector("a.next-button"));
+      }, 50);
+    }, 100);
+  }
+}
+
+function stopNavigation() {
+  clearTimeout(keyPressTimer);
   clearInterval(navigationInterval);
+}
+
+window.addEventListener("keydown", function (evt) {
+  switch (evt.key) {
+    case "ArrowLeft":
+      if (!keyPressTimer && !navigationInterval) {
+        startNavigation("left");
+      }
+      break;
+    case "ArrowRight":
+      if (!keyPressTimer && !navigationInterval) {
+        startNavigation("right");
+      }
+      break;
+    default:
+      break;
+  }
+});
+
+window.addEventListener("keyup", function (evt) {
+  stopNavigation();
   switch (evt.key) {
     case "ArrowLeft":
       goToHref(document.querySelector("a.prev-button"));
@@ -23,28 +60,6 @@ window.addEventListener("keyup", function (evt) {
   }
 });
 
-window.addEventListener("keydown", function (evt) {
-  clearInterval(keyPressTimer);
-  clearInterval(navigationInterval);
-  switch (evt.key) {
-    case "ArrowLeft":
-      keyPressTimer = setTimeout(() => {
-        navigationInterval = setInterval(() => {
-          goToHref(document.querySelector("a.prev-button"));
-        }, 50);
-      }, 450);
-      break;
-    case "ArrowRight":
-      keyPressTimer = setTimeout(() => {
-        navigationInterval = setInterval(() => {
-          goToHref(document.querySelector("a.next-button"));
-        }, 50);
-      }, 450);
-      break;
-    default:
-      break;
-  }
-});
 
 // Handle next TOC select
 function addTocSelectListener() {
