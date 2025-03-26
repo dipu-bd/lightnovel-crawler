1efcc1624e789f70838154d6a3de15f4b33d6ec4
Redowan Ahmed Apu <49404442+redowan99@users.noreply.github.com>
2025-03-26 17:25:33 +0600

    Fix NovelNext (#2595)
    * Added Another Domain
    
    * Fix for TooManyRequest error
    
    * Update novelnext.py
    
    * Fix linting issue
    
    ---------
    
    Co-authored-by: Sudipto Chandra <dipu.sudipta@gmail.com>

--- sources/en/n/novelnext.py
+++ sources/en/n/novelnext.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import logging
 
+from bs4 import BeautifulSoup, Tag
+
 from lncrawl.templates.novelfull import NovelFullTemplate
 
 logger = logging.getLogger(__name__)
 
 
 class NovelNextCrawler(NovelFullTemplate):
-    base_url = ["https://novelnext.com/"]
+    base_url = ["https://novelnext.com/", "https://novelnext.dramanovels.io/"]
 
     def initialize(self) -> None:
+        self.init_executor(ratelimit=0.2)
         self.cleaner.bad_tag_text_pairs.update(
             {
                 "h4": [
@@ -24,3 +27,6 @@ class NovelNextCrawler(NovelFullTemplate):
                 "strong": r"NovelNext\.com",
             }
         )
+
+    def select_chapter_body(self, soup: BeautifulSoup) -> Tag:
+        return soup.select_one("#chr-content, #chapter-content")
