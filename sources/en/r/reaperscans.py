9c57d8b88bbea39e363a22f46d211ca651b8d166
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2025-04-25 02:47:31 +0200

    fix reaperscans crawler

--- sources/en/r/reaperscans.py
+++ sources/en/r/reaperscans.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
-import json
 import logging
-from bs4 import Tag
+from bs4 import BeautifulSoup, Tag
+import re
+import njsparser
 
 from lncrawl.core.crawler import Crawler
+from lncrawl.core.exeptions import LNException
 
 logger = logging.getLogger(__name__)
+logging.getLogger("njsparser").setLevel(logging.ERROR)
+
+CHAPTER_LIST_URL = (
+    "https://api.reaperscans.com/chapters/{}?page={}&perPage=100&order=asc"
+)
 
 
 class Reaperscans(Crawler):
@@ -29,41 +36,6 @@ class Reaperscans(Crawler):
         )
         self.init_executor(ratelimit=0.9)
 
-    def get_chapters_from_page(self, page, body):
-        url = self.absolute_url("/livewire/message/" + body["fingerprint"]["name"])
-        body["updates"] = [
-            {
-                "type": "callMethod",
-                "payload": {
-                    "id": "00000",
-                    "method": "gotoPage",
-                    "params": [page, "page"],
-                },
-            }
-        ]
-
-        response = self.post_json(url=url, data=json.dumps(body), timeout=10)
-        return self.make_soup(response["effects"]["html"])
-
-    def get_chapters_from_doc(self, dom):
-        return [
-            {
-                "title": a.select_one("p").text.strip(),
-                "url": self.absolute_url(a["href"]),
-            }
-            for a in dom.select("div[wire\\3A id] ul[role] li a")
-        ]
-
-    def insert_chapters(self, chapters):
-        self.chapters = [
-            {
-                "id": i + 1,
-                "title": x["title"],
-                "url": x["url"],
-            }
-            for i, x in enumerate(reversed(chapters))
-        ]
-
     def read_novel_info(self):
         logger.debug("Visiting %s", self.novel_url)
         soup = self.get_soup(self.novel_url)
@@ -71,42 +43,53 @@ class Reaperscans(Crawler):
         self.novel_title = soup.select_one("h1").text.strip()
         logger.info("Novel title: %s", self.novel_title)
 
+        match = re.search(r'\\"series_id\\":(\d+)', soup.prettify())
+        if match:
+            self.novel_id = int(match.group(1))
+        else:
+            raise LNException("Could not find series_id in the page source")
+
+        logger.info("Novel ID %d", self.novel_id)
+
         possible_image = soup.select_one(".h-full .w-full img")
         if isinstance(possible_image, Tag):
             self.novel_cover = self.absolute_url(possible_image["src"])
         logger.info("Novel cover: %s", self.novel_cover)
 
-        # livewire container
-        container = soup.select_one("main div[wire\\:id][wire\\:initial-data]")
-        # first page ssr json
-        body = json.loads(container["wire:initial-data"])
-        body.pop("effects")
-        # initial chapters from soup
-        chapters = self.get_chapters_from_doc(container)
-        page_count = 1
-        last_page = container.select_one(
-            'span[wire\\:key^="paginator-page"]:nth-last-child(2)'
-        )
+        chapters = []
+        data = self.get_json(CHAPTER_LIST_URL.format(self.novel_id, 1))
+        chapters += data["data"]
 
-        if isinstance(last_page, Tag):
-            page_count = int(last_page.text.strip())
-        else:
-            self.insert_chapters(chapters)
-            # if we don't have the pagination el
-            return
+        futures = []
+        for page in range(2, data["meta"]["last_page"] + 1):
+            url = CHAPTER_LIST_URL.format(self.novel_id, page)
+            futures.append(self.executor.submit(self.get_json, url))
 
-        toc_futures = [
-            self.executor.submit(self.get_chapters_from_page, k, body)
-            for k in range(2, page_count + 1)
-        ]
-        self.resolve_futures(toc_futures, desc="TOC", unit="page")
-        for f in toc_futures:
-            chapters.extend(self.get_chapters_from_doc(f.result()))
+        for f in futures:
+            data = f.result()
+            chapters += data["data"]
 
-        self.insert_chapters(chapters)
+        for item in chapters:
+            chap_id = 1 + (len(self.chapters))
+            chap_name = item["chapter_name"]
+            self.chapters.append(
+                {
+                    "id": chap_id,
+                    "url": f"{self.novel_url}/{item['chapter_slug']}",
+                    "title": chap_name if chap_name else item["chapter_title"],
+                }
+            )
 
     def download_chapter_body(self, chapter):
-        # TODO: better retry/timeout settings
-        soup = self.get_soup(chapter["url"], retry=3, timeout=10)
-        contents = soup.select_one("article")
-        return self.cleaner.extract_contents(contents)
+        response = self.get_response(chapter["url"], timeout=10)
+        html_text = response.content.decode("utf8", "ignore")
+        fd = njsparser.BeautifulFD(html_text)
+
+        for data in fd.find_iter([njsparser.Text]):
+            if data.value is not None:
+                break
+            else:
+                raise LNException("Could not get chapter content for %s", chapter["url"])
+
+        content = BeautifulSoup(data.value, "lxml")
+        return self.cleaner.extract_contents(content)
