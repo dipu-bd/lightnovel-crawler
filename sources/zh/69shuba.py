3a14759e9b505e6b2354f497875a107bcb8bbf64
unknown <jeremy.guerin34@yahoo.com>
2025-05-22 13:32:41 +0200

    formatting

--- sources/zh/69shuba.py
+++ sources/zh/69shuba.py
@@ -5,8 +5,6 @@ from bs4 import Tag
 from lncrawl.core.crawler import Crawler
 import urllib.parse
 
-from lncrawl.models import Volume, Chapter
-
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,"
