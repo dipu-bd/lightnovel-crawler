2dcce4e2c374ac3169f1cd5b33860cc7d80cb1dc
unknown <jeremy.guerin34@yahoo.com>
2025-05-24 21:20:27 +0200

    fix headers

--- sources/zh/xbanxia.py
+++ sources/zh/xbanxia.py
@@ -7,12 +7,9 @@ logger = logging.getLogger(__name__)
 search_url = "https://www.banxia.cc/modules/article/search_t.php"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
     "Content-Type": "application/x-www-form-urlencoded",
     "Origin": "https://www.banxia.cc",
     "Referer": "https://www.banxia.cc/",
-    "Connection": "keep-alive",
     "Cookie": "jieqiUserCharset=utf-8;",
 }
 
