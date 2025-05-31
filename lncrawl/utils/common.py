64bd8ce26935aade778f6aaf2bd8ba22baf96a03
Sudipto Chandra <dipu.sudipta@gmail.com>
2025-05-31 15:51:21 +0400

    Fixes types

--- lncrawl/utils/common.py
+++ lncrawl/utils/common.py
@@ -1,14 +1,17 @@
-from typing import TypeVar, Generic, Callable
+from typing import TypeVar, Generic, Callable, Type
 
 T = TypeVar('T')
 
 
 class static_cached_property(Generic[T]):
     def __init__(self, func: Callable[..., T]):
-        self.func = func
         self._initialized = False
+        if isinstance(func, staticmethod):
+            self.func = func.__func__
+        else:
+            self.func = func
 
-    def __get__(self, instance, owner) -> T:
+    def __get__(self, instance: None, owner: Type) -> T:
         if not self._initialized:
             self._value = self.func()
             self._initialized = True
