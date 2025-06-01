19f9ad11c764e4b061fa7c6e8cc08e9c766e320f
Sudipto Chandra <dipu.sudipta@gmail.com>
2025-06-01 07:43:42 +0400

    Remove accept header

--- scripts/check_sources.py
+++ scripts/check_sources.py
@@ -49,7 +49,6 @@ def main():
             session.get, url, timeout=5, stream=True,
             headers={
                 'User-Agent': user_agents[i % len(user_agents)],
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             }
         )
 
