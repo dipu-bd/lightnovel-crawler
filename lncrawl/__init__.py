0ae08f8f8179eee8ddf807dfc370e8fa90bdc97f
Sudipto Chandra <dipu.sudipta@gmail.com>
2025-06-27 09:31:34 +0400

    Update __init__.py

--- lncrawl/__init__.py
+++ lncrawl/__init__.py
@@ -6,7 +6,7 @@ import multiprocessing
 
 def main():
     multiprocessing.freeze_support()
-    multiprocessing.set_start_method("spawn")
+    # multiprocessing.set_start_method("spawn")
 
     try:
         from dotenv import load_dotenv
