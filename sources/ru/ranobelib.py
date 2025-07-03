983b045472f67ce65c814267a749e8ecc6ef1d66
CryZFix <cryzfix@gmail.com>
2025-07-03 23:56:06 +0400

    restored ranobelib.me

--- sources/ru/ranobelib.py
+++ sources/ru/ranobelib.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
-import json
 import operator
+from urllib.parse import urlencode
 
-from bs4 import BeautifulSoup
 from lncrawl.core.crawler import Crawler
 
 logger = logging.getLogger(__name__)
@@ -15,88 +14,123 @@ class RanobeLibMeCrawler(Crawler):
     ]
 
     def initialize(self):
-        self.init_executor(1)
+        self.init_executor(ratelimit=0.99)
+        clean_url = self.novel_url.split("?")[0].strip("/")
+        self.api_url = f"https://api.cdnlibs.org/api/manga/{clean_url.split('/')[-1]}"
 
     def read_novel_info(self):
-        clean_url = self.novel_url.split("?")[0].strip("/")
-        logger.debug("Visiting %s", self.novel_url)
-        soup = self.get_soup(f"{clean_url}?section=info")
+        api_requests_params = {
+            "fields[]": [
+                "background",
+                "eng_name",
+                "otherNames",
+                "summary",
+                "genres",
+                "chap_count",
+                "status_id",
+                "authors",
+                "format",
+            ]
+        }
 
-        content = self.extract_page_data(soup)
-        self.novel_title = content["manga"]["rusName"]
+        logger.debug("Visiting %s", self.novel_url)
+        book_info = self.get_json(
+            f"{self.api_url}?{urlencode(api_requests_params, doseq=True)}"
+        )
+
+        self.novel_title = book_info["data"]["rus_name"]
         logger.info("Novel title: %s", self.novel_title)
 
-        possible_image = soup.select_one(".media-sidebar__cover > img:nth-child(1)")
-        if possible_image:
-            self.novel_cover = self.absolute_url(possible_image["src"])
+        novel_cover = book_info["data"]["cover"]["default"]
+        if "https" not in novel_cover:
+            novel_cover = f"https://ranobelib.me/{novel_cover}"
+        self.novel_cover = novel_cover
         logger.info("Novel cover: %s", self.novel_cover)
 
-        for list_value in soup.find_all("div", {"class": "media-info-list__value"}):
-            possible_author_ref = list_value.find("a")
-            if not possible_author_ref:
-                continue
-            if "ranobelib.me/people" not in possible_author_ref["href"]:
-                continue
-            self.novel_author = possible_author_ref.text.strip()
-            break
+        self.novel_author = book_info["data"]["authors"][0]["name"]
         logger.info("Novel author: %s", self.novel_author)
 
-        self.novel_synopsis = self.cleaner.extract_contents(
-            soup.find("div", {"class": "media-description__text"})
-        )
+        self.novel_synopsis = book_info["data"]["summary"]
         logger.info("Novel synopsis: %s", self.novel_synopsis)
 
-        for tag in soup.find_all("a", {"class": "media-tag-item"}):
-            self.novel_tags.append(tag.text)
+        genres = book_info["data"]["genres"]
+        self.novel_tags = [item["name"] for item in genres]
         logger.info("Novel tags: %s", self.novel_tags)
 
-        chapters = content["chapters"]["list"]
-        chapters.reverse()
+        chapters = self.get_json(f"{self.api_url}/chapters")["data"]
         chap_id = 0
-        volumes_set = set()
 
         branches = dict()
         for chapter in chapters:
-            key = chapter["branch_id"]
+            for branch in chapter["branches"]:
+                key = branch["branch_id"]
             branches[key] = branches.setdefault(key, 0) + 1
         branch = max(branches.items(), key=operator.itemgetter(1))[0]
+        if not isinstance(branch, int):
+            branch = 0
 
         for chapter in chapters:
-            if chapter["branch_id"] != branch:
+            if any(
+                "moderation" in chapter_branch for chapter_branch in chapter["branches"]
+            ):
                 continue
 
-            chap_id = chap_id + 1
-            chap_num = chapter["chapter_number"]
-            vol_id = chapter["chapter_volume"]
+            ### Left it temporarily, to be fixed later
+            # if not any(
+            #     chapter_branch["branch_id"] == branch
+            #     for chapter_branch in chapter["branches"]
+            # ):
+            #     continue
 
-            if vol_id not in volumes_set:
-                volumes_set.add(vol_id)
-                self.volumes.append({"id": vol_id})
+            chap_id = chap_id + 1
+            chap_num = chapter["number"]
+
+            params = {
+                "volume": chapter["volume"],
+                "number": chapter["number"],
+                "branch_id": branch,
+            }
 
             self.chapters.append(
                 {
                     "id": chap_id,
-                    "volume": vol_id,
-                    "url": self.absolute_url(
-                        f"{clean_url}/v{str(vol_id)}/c{chap_num}/"
-                    ),
-                    "title": chapter["chapter_name"] or (f"Глава {chap_num}"),
+                    "url": f"{self.api_url}/chapter?{urlencode(params, doseq=True)}",
+                    "title": chapter["name"] or (f"Глава {chap_num}"),
                 }
             )
 
     def download_chapter_body(self, chapter):
-        soup = self.get_soup(chapter["url"])
-        contents = soup.select_one(".reader-container")
-        self.cleaner.clean_contents(contents)
-        return str(contents)
+        chapter = self.get_json(chapter["url"])
+        chapter_content = ""
 
-    def extract_page_data(self, soup: BeautifulSoup) -> dict:
-        for script in soup.find_all("script"):
-            json_var = "window.__DATA__ = "
-            text = script.text.strip()
-            if not text or not text.startswith(json_var):
-                continue
-            text = text[len(json_var) : text.find("window._SITE_COLOR_")].strip(
-                ";\t\n "
+        if "content" in chapter["data"]["content"]:
+            paragraphs = self._paragraph_parser(data=chapter["data"])
+            chapter_content = "".join(
+                [
+                    f"<p>{text['text']}</p>"
+                    for tag in paragraphs
+                    for text in tag
+                    if "text" in text
+                ]
             )
-            return json.loads(text)
+            return chapter_content
+
+        else:
+            chapter_content = self.make_soup(chapter["data"]["content"])
+            return self.cleaner.extract_contents(chapter_content)
+
+    def _paragraph_parser(self, data):
+        paragraphs = []
+
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key == "type" and value == "paragraph":
+                    paragraphs.append(data.get("content", ""))
+                else:
+                    paragraphs.extend(self._paragraph_parser(value))
+
+        elif isinstance(data, list):
+            for item in data:
+                paragraphs.extend(self._paragraph_parser(item))
+
+        return paragraphs
