98a092c2afc0cc07f751a822425c97599ff42250
Sudipto Chandra <dipu.sudipta@gmail.com>
2025-07-07 12:27:10 +0400

    Update novelfire.py

--- sources/en/n/novelfire.py
+++ sources/en/n/novelfire.py
@@ -2,37 +2,27 @@ import logging
 from typing import List
 
 from lncrawl.core.crawler import Crawler
-from lncrawl.models import Chapter, SearchResult
 
 logger = logging.getLogger(__name__)
 
 
 class NovelFireCrawler(Crawler):
-
-    base_url = ["https://novelfire.net/"]
-
+    base_url = [
+        "https://novelfire.net/",
+    ]
     has_mtl = False
-
     has_mange = False
 
     def initialize(self) -> None:
         self.init_executor(ratelimit=3)
 
-    # TODO: [OPTIONAL] Return a list of search results using the query.
-    def search_novel(self, query) -> List[SearchResult]:
-        # You may raise an Exception or return empty list in case of failure.
-        pass
-
     def read_novel_info(self) -> None:
-
         soup = self.get_soup(self.novel_url)
 
         self.novel_title = soup.find("h1").text.strip()
-
         self.novel_author = soup.select_one('span[itemprop="author"]').text.strip()
 
         img = soup.select_one(".cover img")
-
         self.novel_cover = self.absolute_url(img["data-src"])
 
         vol_id = 1
@@ -42,13 +32,16 @@ class NovelFireCrawler(Crawler):
             soup = self.get_soup(self.absolute_url(vol_url))
 
             chapters = soup.select("ul.chapter-list li a")
-
             for a in chapters:
                 chap_id = len(self.chapters) + 1
+                self.chapters.append({
+                    "id": chap_id,
+                    "volume": vol_id,
+                    "title": a["title"],
+                    "url": self.absolute_url(a["href"]),
+                })
 
-                self.chapters.append({"id": chap_id, "volume": vol_id, "title": a["title"], "url": self.absolute_url(a["href"])})
-
-            self.volumes.append({"id": vol_id})
+            self.volumes.append({ "id": vol_id })
 
             next_vol_a = soup.select_one("a.page-link[rel='next']")
             if next_vol_a:
@@ -58,9 +51,7 @@ class NovelFireCrawler(Crawler):
                 vol_url = False
                 break
 
-    def download_chapter_body(self, chapter: Chapter) -> str:
+    def download_chapter_body(self, chapter) -> str:
         soup = self.get_soup(chapter["url"])
-
         contents = soup.select_one("div#content")
-
         return self.cleaner.extract_contents(contents)
