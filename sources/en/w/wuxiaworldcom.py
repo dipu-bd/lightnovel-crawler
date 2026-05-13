76eef9f29b2c3402090358a29d3b226e812fd3dc
Server Admin <admin>
2026-05-13 22:55:29 +0400

    Update source: wuxiaworld.com

--- sources/en/w/wuxiaworldcom.py
+++ sources/en/w/wuxiaworldcom.py
@@ -64,8 +64,11 @@ class WuxiaworldComCrawler(BrowserTemplate):
                 )
                 self.browser.find("//h2[normalize-space()='Your Profile']", By.XPATH)
                 if self.browser.local_storage.has(self.localstorageuser):
+                    user = self.browser.local_storage.get(self.localstorageuser)
+                    if not user:
+                        raise Exception("No localstorageuser in localstorage")
                     self.bearer_token = "{token_type} {access_token}".format(
-                        **json.loads(self.browser.local_storage[self.localstorageuser])
+                        **json.loads(user)
                     )
             except Exception as e:
                 logger.info("login Email: Failed", e)
