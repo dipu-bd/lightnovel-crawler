873e02b7aa5d82a5e5df0f0e174caf5a78cdf1f4
Server Admin <lncrawl@pm.me>
2026-05-16 20:46:06 +0400

    Update source: wattpad.com

--- sources/multi/wattpad.py
+++ sources/multi/wattpad.py
@@ -46,7 +46,9 @@ class WattpadCrawler(LegacyCrawler):
         id_no = search_id.search(self.novel_url)
         if not id_no:
             raise LNException("No story ID found")
-        response = self.get_response(f"{self.scraper.origin}api/v3/stories/{id_no.group()}")
+        response = self.get_response(
+            f"{self.scraper.origin}api/v3/stories/{id_no.group()}"
+        )
         story_info = response.json()
 
         self.novel_title = story_info["title"]
