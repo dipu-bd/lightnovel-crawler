a9e31dca1aa6bb5e0585bce40d5c82c2c412364e
Temple <temple@templeofshadow.org>
2026-06-11 11:42:47 -0500

    botitranslation: last before switching to legacycrawler (non-functional state)

--- sources/en/b/botitranslation.py
+++ sources/en/b/botitranslation.py
@@ -2,44 +2,28 @@
 import logging
 from typing import Generator, Optional, Union
 
-from bs4 import BeautifulSoup, Tag
-
-from lncrawl.models import Chapter, Volume
-from lncrawl.templates.soup.general import GeneralSoupTemplate
+from lncrawl.core import Chapter, Novel, PageSoup, SoupTemplate, Volume, BrowserTemplate
 
 logger = logging.getLogger(__name__)
 
 
-class BotiTranslationCrawler(GeneralSoupTemplate):
+class BotiTranslationCrawler(SoupTemplate):
     base_url = "https://www.botitranslation.com/"
 
-    def parse_title(self, soup: BeautifulSoup) -> str:
-        """Return the novel title from the novel page."""
-        #raise NotImplementedError()  # e.g. return soup.select_one("h1.title").get_text(strip=True)
-        return soup.select_one("div.book-name span").get_text()
+    novel_title_selector = ".info-section .book-name span"
+    novel_author_selector = ".info-section .author-name"
+    novel_cover_selector = ".cover img.cover-img[src]"
 
-    def parse_cover(self, soup: BeautifulSoup) -> Optional[str]:
-        """Return the cover image URL, or '' if none."""
-
-        #return ""
-
-        return soup.select_one("div.cover img.cover-img").get_text()
+    chapter_body_selector = ".chapter-content"
 
     def parse_chapter_list(
-        self, soup: BeautifulSoup
+        self, soup: PageSoup, novel: Novel
     ) -> Generator[Union[Chapter, Volume], None, None]:
-        links = soup.select("tr td a")
-        for idx, a in enumerate(links, 1):
+        links = list(soup.select(".container div#toc-panel table tbody tr td a[href]"))
+        for idx, a in enumerate(links, 0):
             yield Chapter(
                 id=idx,
                 title=a.get_text(strip=True),
                 url=self.absolute_url(a["href"]),
             )
         logger.info("Found %d chapters", len(links))
-
-    def select_chapter_body(self, soup: BeautifulSoup) -> Optional[Tag]:
-        """Return the Tag that contains the chapter text (soup is the chapter page)."""
-
-        return soup.select_one("div.chapter-content")
-
-        #raise NotImplementedError()  # e.g. return soup.select_one(".chapter-content")
