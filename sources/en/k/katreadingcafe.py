64bd38ad2328f3c6b798be89a7efc543c79ce870
Sudipto Chandra <dipu.sudipta@gmail.com>
2026-06-12 20:50:39 +0400

    Update katreadingcafe.py

--- sources/en/k/katreadingcafe.py
+++ sources/en/k/katreadingcafe.py
@@ -146,8 +146,10 @@ class KatReadingCafeCrawler(SoupTemplate):
 
     def _valid_chapter_link(self, link: PageSoup) -> bool:
         href = link.get("href")
-        stripped_base_url = str(self.base_url).strip("[]'")
-        if stripped_base_url not in href:
+        for url in self.base_url:
+            if url in href: # found base url in the href
+                break
+        else: # href is a foreign url
             return False
         if "🔒" in link.text:  # locked chapter
             return False
