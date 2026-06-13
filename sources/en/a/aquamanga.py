62e1b3f56efcfd2c74defa1d2b92d36a02ee3f7c
Server Admin <lncrawl@pm.me>
2026-06-14 02:08:46 +0400

    Update source: aquareader.net

--- sources/en/a/aquamanga.py
+++ sources/en/a/aquamanga.py
@@ -3,7 +3,12 @@ from lncrawl.templates.wordpress import WordpressMangaTemplate
 
 
 class AquaMangaCrawler(WordpressMangaTemplate):
-    base_url = ["https://aquamanga.com/", "https://aquamanga.org/", "https://aquareader.net/"]
+    base_url = [
+        "https://aquamanga.com/",
+        "https://aquamanga.org/",
+        "https://aquareader.net/",
+        "https://aquareader.org/",
+    ]
 
     def initialize(self) -> None:
         self.cleaner.bad_tags.update(["h3"])
