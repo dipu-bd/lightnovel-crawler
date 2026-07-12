a7ebffc1bf6d7430552db5786cb752edbaf1a798
Sudipto Chandra <dipu.sudipta@gmail.com>
2026-07-12 18:47:10 +0400

    Update tag.py

--- lncrawl/dao/tag.py
+++ lncrawl/dao/tag.py
@@ -16,7 +16,5 @@ class NovelTag(sa.SQLModel, table=True):
         sa.Index("ix_novel_tag_name", "tag_name"),
     )
 
-    novel_id: str = sa.Field(
-        foreign_key="novels.id", ondelete="CASCADE", primary_key=True
-    )
+    novel_id: str = sa.Field(foreign_key="novels.id", ondelete="CASCADE", primary_key=True)
     tag_name: str = sa.Field(primary_key=True, description="Tag name")
