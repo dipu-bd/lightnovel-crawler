3aeee39e6b48bd5ea4cd228fe937c067f46c8bea
Sudipto Chandra <dipu.sudipta@gmail.com>
2026-07-12 18:48:11 +0400

    Fix table_args formatting in NovelTag class

--- lncrawl/dao/tag.py
+++ lncrawl/dao/tag.py
@@ -12,9 +12,7 @@ class Tag(sa.SQLModel, table=True):
 
 class NovelTag(sa.SQLModel, table=True):
     __tablename__ = "novel_tags"  # type: ignore
-    __table_args__ = (
-        sa.Index("ix_novel_tag_name", "tag_name"),
-    )
+    __table_args__ = (sa.Index("ix_novel_tag_name", "tag_name"),)
 
     novel_id: str = sa.Field(foreign_key="novels.id", ondelete="CASCADE", primary_key=True)
     tag_name: str = sa.Field(primary_key=True, description="Tag name")
